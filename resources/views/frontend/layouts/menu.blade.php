7fb2f8232c8020bc3a52e73acc1470ccd3055a26
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-06-24 16:34:51 +0530

    Update menu.blade.php

--- resources/views/frontend/layouts/menu.blade.php
+++ resources/views/frontend/layouts/menu.blade.php
@@ -72,19 +72,19 @@
                             @endforeach
                         @endif
                     </ul>
-                    <ul class="d-flex menu_icon">
+                    <ul class="d-flex menu_icon my-3">
                         <li>
                             <a class="fs-5" href="{{ route('login') }}"><i class="fas fa-user"></i></a>
                         </li>
                         <li>
-                            <a class="cart_icon mx-3 position-relative fw-semibold fs-5 transitions"><i
+                            <a class="cart_icon mx-4 position-relative fw-semibold fs-5 transitions"><i
                                     class="fas fa-shopping-basket"></i>
                                 <span
                                     class="cart_count bg-gray position-absolute text-light fw-semibold text-center bottom-50 end-25 rounded-circle">{{ count(Cart::content()) }}</span></a>
                         </li>
                         @php @$unseenMessages = \App\Models\Chat::where(['sender_id' => 1, 'receiver_id' => auth()->user()->id, 'seen' => 0])->count(); @endphp
                         <li>
-                            <a class="message_icon mx-3 position-relative fw-semibold fs-5 transitions"
+                            <a class="message_icon mx-4 position-relative fw-semibold fs-5 transitions"
                                 href="{{ route('dashboard') }}">
                                 <i class="fas fa-comment-alt-dots"></i>
                                 <span
@@ -92,7 +92,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="menu_search mx-3 position-relative fw-semibold fs-5 transitions"><i
+                            <a href="#" class="menu_search mx-4 position-relative fw-semibold fs-5 transitions"><i
                                     class="far fa-search"></i></a>
                             <div
                                 class="fp__search_form position-fixed w-100 top-0 end-0 justify-content-center align-items-center transitions vh-100">
