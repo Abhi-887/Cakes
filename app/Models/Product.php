beed0eeb57415d28ff9528a02b3384a10e95ed4a
Abhishek Rajput <blazeneon1@gmail.com>
2024-06-24 15:40:13 +0530

    t

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,23 +11,35 @@ class Product extends Model
 {
     use HasFactory;
 
-    function category() : BelongsTo {
+    function category(): BelongsTo
+    {
         return $this->belongsTo(Category::class);
     }
 
-    function productImages() : HasMany {
+    function productImages(): HasMany
+    {
         return $this->hasMany(ProductGallery::class);
     }
 
-    function productSizes() : HasMany {
+    function productSizes(): HasMany
+    {
         return $this->hasMany(ProductSize::class);
     }
 
-    function productOptions() : HasMany {
+    function productOptions(): HasMany
+    {
         return $this->hasMany(ProductOption::class);
     }
 
-    function reviews() : HasMany {
+    function reviews(): HasMany
+    {
         return $this->hasMany(ProductRating::class, 'product_id', 'id');
     }
+
+    // In your Product model (App\Models\Product)
+    public function subCategory()
+    {
+        return $this->belongsTo(Category::class, 'sub_category');
+    }
+
 }
