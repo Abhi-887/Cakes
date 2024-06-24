338f2fddde2b3c2541a18c19994807896dbabb05
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-06-24 15:01:18 +0530

    Update menu.blade.php

--- resources/views/frontend/layouts/menu.blade.php
+++ resources/views/frontend/layouts/menu.blade.php
@@ -77,7 +77,7 @@
 
             <ul class="menu_icon d-flex align-items-center flex-wrap">
                 <li>
-                    <a href="#" class="menu_search mx-4 position-relative fw-semibold fs-5 transitions"><i
+                    <a href="#" class="menu_search mx-3 position-relative fw-semibold fs-5 transitions"><i
                             class="far fa-search"></i></a>
                     <div
                         class="fp__search_form position-fixed w-100 top-0 end-0 justify-content-center align-items-center transitions vh-100">
@@ -97,23 +97,23 @@
                     </div>
                 </li>
                 <li>
-                    <a class="cart_icon mx-4 position-relative fw-semibold fs-5 transitions"><i
+                    <a class="cart_icon mx-3 position-relative fw-semibold fs-5 transitions"><i
                             class="fas fa-shopping-basket"></i>
                         <span
                             class="cart_count bg-gray position-absolute text-light fw-semibold text-center bottom-50 end-25 rounded-circle">{{ count(Cart::content()) }}</span></a>
                 </li>
                 @php @$unseenMessages = \App\Models\Chat::where(['sender_id' => 1, 'receiver_id' => auth()->user()->id, 'seen' => 0])->count(); @endphp
-                <li>
-                    <a class="message_icon mx-4 position-relative fw-semibold fs-5 transitions"
+                {{-- <li>
+                    <a class="message_icon mx-3 position-relative fw-semibold fs-5 transitions"
                         href="{{ route('dashboard') }}">
                         <i class="fas fa-comment-alt-dots"></i>
                         <span
                             class="sunseen-message-count bg-gray position-absolute text-light fw-semibold text-center bottom-50 end-25 ms-1 rounded-circle">{{ $unseenMessages > 0 ? 1 : 0 }}</span>
                     </a>
-                </li>
+                </li> --}}
 
                 <li>
-                    <a class="ms-4 fs-5" href="{{ route('login') }}"><i class="fas fa-user"></i></a>
+                    <a class="ms-3 fs-5" href="{{ route('login') }}"><i class="fas fa-user"></i></a>
                 </li>
 
                 {{--
