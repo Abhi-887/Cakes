0ec5df780a426665aa21abbb3524a85257fea68a
drupalbuilder-shubham <133983331+drupalbuilder-shubham@users.noreply.github.com>
2024-06-24 12:21:35 +0530

    fhg

---
+++ app/DataTables/WorkwithusDataTable.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\Workwithus;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class WorkwithusDataTable extends DataTable
+{
+    /**
+     * Build the DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->addColumn('action', 'subscriber.action')
+            ->setRowId('id');
+    }
+
+    /**
+     * Get the query source of dataTable.
+     */
+    public function query(Workwithus $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use the html builder.
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+                    ->setTableId('Workwithus-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    //->dom('Bfrtip')
+                    ->orderBy(0)
+                    ->selectStyleSingle()
+                    ->buttons([
+                        Button::make('excel'),
+                        Button::make('csv'),
+                        Button::make('pdf'),
+                        Button::make('print'),
+                        Button::make('reset'),
+                        Button::make('reload')
+                    ]);
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     */
+    public function getColumns(): array
+    {
+        return [
+            Column::make('id'),
+            Column::make('job_reference'),
+            Column::make('name'),
+            Column::make('email'),
+            Column::make('telephone'),
+            Column::make('driving_license'),
+            Column::make('why_ideal'),
+            Column::make('relevant_experience'),
+            Column::make('current_position_duration'),
+            Column::make('portfolio'),
+            Column::make('cv'),
+            Column::make('created_at'),
+            Column::make('updated_at'),
+        ];
+    }
+
+
+    /**
+     * Get the filename for export.
+     */
+    protected function filename(): string
+    {
+        return 'Workwithus_' . date('YmdHis');
+    }
+}
