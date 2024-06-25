a1a0d00fe1abbd3b91afb9cfc70eee3c3be454c6
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-06-25 16:19:20 +0530

    Update contact.blade.php

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            BREADCRUMB START
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    BREADCRUMB START
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ==============================-->
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay">
             <div class="container">
@@ -18,13 +18,13 @@
         </div>
     </section>
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                BREADCRUMB END
-                                                                                                                                                                                                                                                                                                                                                        ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                        BREADCRUMB END
+                                                                                                                                                                                                                                                                                                                                                                                ==============================-->
 
 
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                CONTACT PAGE START
-                                                                                                                                                                                                                                                                                                                                                        ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                        CONTACT PAGE START
+                                                                                                                                                                                                                                                                                                                                                                                ==============================-->
 
 
     <style>
@@ -177,7 +177,7 @@
             <div class="contact-card py-5">
                 <h2 class="color-dark-gray text-center fw-bold">Opening Times</h2>
                 <div class="row pt-5">
-                    <div class="col-md-4 d-flex justify-content-md-center text-center text-md-start">
+                    <div class="col-md-4 d-flex justify-content-center text-center text-md-start">
                         <div>
                             <h4 class="color-light-gray mb-3">Cake orders & collections:</h4>
                             <p class="fw-semibold">Monday - Friday 9am - 5pm (please call 0131 337 9990 upon arrival if
@@ -273,7 +273,7 @@
             <div class="contact-card py-5">
                 <h2 class="color-dark-gray text-center fw-bold">Opening Times</h2>
                 <div class="row pt-5">
-                    <div class="col-md-4 d-flex justify-content-md-center text-center text-md-start">
+                    <div class="col-md-4 d-flex justify-content-center text-center text-md-start">
                         <div>
                             <h4 class="color-light-gray mb-3">Cake orders & collections:</h4>
                             <p class="fw-semibold">Monday - Friday 9am - 5pm<br>Saturday: 9am - 4pm<br>Sunday: 10am - 3pm
@@ -281,13 +281,13 @@
                                 appointment only)</p>
                         </div>
                     </div>
-                    <div class="col-md-4 d-flex justify-content-md-center text-center text-md-start">
+                    <div class="col-md-4 d-flex justify-content-center text-center text-md-start">
                         <div>
                             <h4 class="color-light-gray mb-3">Coffee & Cake:</h4>
                             <p class="fw-semibold">Monday - Friday 9am - 4pm<br>Saturday: 9am - 4pm<br>Sunday: Closed</p>
                         </div>
                     </div>
-                    <div class="col-md-4 d-flex justify-content-md-center text-center text-md-start">
+                    <div class="col-md-4 d-flex justify-content-center text-center text-md-start">
                         <div>
                             <h4 class="color-light-gray mb-3">7-Day Cake Delivery:</h4>
                             <p class="fw-semibold">Cake design consultations are available<br>7 days a week by appointment
@@ -356,7 +356,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 wow fadeInUp" data-wow-duration="1s">
+            <div class="col-md-6 mt-5 mt-md-0 wow fadeInUp" data-wow-duration="1s">
                 <div class="formbg rounded-4">
                     <form class="formbg fp__contact_form">
                         @csrf
@@ -395,8 +395,8 @@
         </div>
     </div>
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            CONTACT PAGE END
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    CONTACT PAGE END
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ==============================-->
 @endsection
 
 @push('scripts')
