8e710a6a3657c39d87b22b923eb20124c607fd74
Rahul <rahul@drupalbuilder.com>
2024-06-25 16:35:15 +0530

    update

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -12,9 +12,18 @@ class Contact extends Model
     protected $fillable = [
         'phone_one',
         'phone_two',
+        'phone_image',
         'mail_one',
         'mail_two',
+        'email_image',
         'address',
-        'map_link'
+        'map_link',
+        'title_one',
+        'Description_one',
+        'title_two',
+        'Description_two',
+        'title_three',
+        'Description_three',
+
     ];
 }
