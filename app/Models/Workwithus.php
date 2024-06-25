f1c2f39eb12654ce0a9bb4a3768fa164d20bf4a9
drupalbuilder-shubham <133983331+drupalbuilder-shubham@users.noreply.github.com>
2024-06-25 11:55:14 +0530

    Update Workwithus.php

--- app/Models/Workwithus.php
+++ app/Models/Workwithus.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Customerfeedback extends Model
+class Workwithus extends Model
 {
     use HasFactory;
 
