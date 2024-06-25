f5ee8f0ad9e072100add6d67dbcafa93dbfed9ac
Rahul <rahul@drupalbuilder.com>
2024-06-25 15:38:51 +0530

    Update index.blade.php

--- resources/views/admin/contact2/index.blade.php
+++ resources/views/admin/contact2/index.blade.php
@@ -18,12 +18,18 @@
 
                     <div class="form-group">
                         <label for="">Phone One</label>
-                        <input type="text" class="form-control" name="phone_one" value="{{ @$contact->phone_one }}">
+                        <textarea name="text"   class="form-control summernote">{{@$contact->phone_one}}</textarea>
                     </div>
 
                     <div class="form-group">
                         <label for="">Phone Two</label>
-                        <input type="text" class="form-control" name="phone_two" value="{{ @$contact->phone_two }}">
+
+                        <textarea name="text"   class="form-control summernote">{{@$contact->phone_two}}</textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">Phone Image</label>
+                        <input type="file" class="form-control" name="image" value="{{ @$contact->image }}">
                     </div>
 
                     <div class="form-group">
@@ -34,6 +40,11 @@
                     <div class="form-group">
                         <label for="">Email Two</label>
                         <input type="text" class="form-control" name="mail_two" value="{{ @$contact->mail_two }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">Email Image</label>
+                        <input type="file" class="form-control" name="image" value="{{ @$contact->image }}">
                     </div>
 
                     <div class="form-group">
