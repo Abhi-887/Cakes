bdbc8591d306e1596441754e28d1ab2fb218db36
drupalbuilder-saloni <shubham@drupalbuilder.com>
2024-06-26 08:55:56 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware('auth:sanctum')->get('/products', function () {
+Route::get('/products', function () {
     $products = Product::all();
     return $products;
 });
