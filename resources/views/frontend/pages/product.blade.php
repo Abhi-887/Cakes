ebbf08585affc519690237390c3d8ce6d62ce4b6
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-07-01 18:08:27 +0530

    Update product.blade.php

--- resources/views/frontend/pages/product.blade.php
+++ resources/views/frontend/pages/product.blade.php
@@ -40,27 +40,30 @@
                     <div>
                         <a href="/consultations">
                             <img class="rounded-3"
-                                src="https://www.3d-cakes.co.uk/media/wysiwyg/boxes/categories/weddings/Book-a-free-consultation.jpg"
+                                src="https://img.freepik.com/free-photo/couple-cafe-looking-smartphone-screen_23-2147826834.jpg?t=st=1719837254~exp=1719840854~hmac=15c7d8cafe8add60ed237d56c93ba7960b4a813b23ec853c77ea4a37ef4e5a18&w=1380"
                                 alt="">
                         </a>
+                        <h3>Book Free Consultation</h3>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div>
                         <a href="/wedding-cake-deposit">
                             <img class="rounded-3"
-                                src="https://www.3d-cakes.co.uk/media/wysiwyg/boxes/categories/weddings/3D_BOX_-_why-choose-3d-cakes.jpg"
+                                src="https://img.freepik.com/free-photo/side-view-bride-wearing-wedding-ring_23-2149617163.jpg?t=st=1719837397~exp=1719840997~hmac=5d5a8bfccc56431f4d85a9160271a843815bd7b32d340bc7366e4b659feea537&w=1380"
                                 alt="">
                         </a>
+                        <h3>Secure Your Wedding Date Here</h3>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div>
                         <a href="">
                             <img class="rounded-3"
-                                src="https://www.3d-cakes.co.uk/media/wysiwyg/boxes/categories/weddings/flavour-guide-new.png"
+                                src="https://img.freepik.com/free-photo/close-uf-delicious-souffle-dessert-decorated-with-raspberies-fresh-mint-leaves-served-white-square-plate-photo-was-taken-black-wall-tasty-sweet-buffet-catering_7502-5866.jpg?t=st=1719837466~exp=1719841066~hmac=d62696a58bf5362289af12753885c687f1d111b2b0841b9363263bded96130c9&w=1380"
                                 alt="">
                         </a>
+                        <h3>Delisious Flavour Cake</h3>
                     </div>
                 </div>
             </div>
