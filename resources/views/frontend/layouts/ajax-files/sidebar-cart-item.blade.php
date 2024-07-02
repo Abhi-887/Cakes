f31f16785f309ae33938c027dd7d88b93b868e63
Abhishek Rajput <blazeneon1@gmail.com>
2024-07-02 17:56:53 +0530

    s

--- resources/views/frontend/layouts/ajax-files/sidebar-cart-item.blade.php
+++ resources/views/frontend/layouts/ajax-files/sidebar-cart-item.blade.php
@@ -2,62 +2,28 @@
 <input type="hidden" value="{{ count(Cart::content()) }}" id="cart_product_count">
 
 @foreach (Cart::content() as $cartProduct)
-<<<<<<< HEAD
 <li>
     <div class="menu_cart_img">
-        @if (isset($cartProduct->options->product_info['image']))
         <img src="{{ asset($cartProduct->options->product_info['image']) }}" alt="menu" class="img-fluid w-100">
-        @else
-        <img src="{{ asset('path_to_default_image.jpg') }}" alt="Default Image" class="img-fluid w-100">
-        @endif
     </div>
     <div class="menu_cart_text">
-        <a class="title" href="{{ route('product.show', $cartProduct->options->product_info['slug'] ?? '') }}">
-            {!! $cartProduct->name !!}
+        <a class="title" href="{{ route('product.show', $cartProduct->options->product_info['slug']) }}">{!!
+            $cartProduct->name !!}
         </a>
         <p class="size">Qty: {{ $cartProduct->qty }}</p>
 
-        @if (isset($cartProduct->options->product_size))
-        <p class="size">{{ $cartProduct->options->product_size['name'] }}
-            {{ $cartProduct->options->product_size['price'] ? '(' .
-            currencyPosition($cartProduct->options->product_size['price']) . ')' : '' }}
+        <p class="size">{{ @$cartProduct->options->product_size['name'] }}
+            {{ @$cartProduct->options->product_size['price'] ? '(' .
+            currencyPosition(@$cartProduct->options->product_size['price']) . ')' : '' }}
         </p>
-        @endif
 
-        @if (isset($cartProduct->options->product_options) && is_array($cartProduct->options->product_options))
         @foreach ($cartProduct->options->product_options as $cartProductOption)
-        <span class="extra">{{ $cartProductOption['name'] }} ({{ currencyPosition($cartProductOption['price'])
-            }})</span>
+        <span class="extra">{{ $cartProductOption['name'] }}
+            ({{ currencyPosition($cartProductOption['price']) }})</span>
         @endforeach
-        @endif
-
         <p class="price">{{ currencyPosition($cartProduct->price) }}</p>
     </div>
     <span class="del_icon" onclick="removeProductFromSidebar('{{ $cartProduct->rowId }}')"><i
             class="fal fa-times"></i></span>
 </li>
-=======
-    <li>
-        <div class="menu_cart_img">
-            <img src="{{ asset($cartProduct->options->product_info['image']) }}" alt="menu" class="img-fluid w-100">
-        </div>
-        <div class="menu_cart_text">
-            <a class="title"
-                href="{{ route('product.show', $cartProduct->options->product_info['slug']) }}">{!! $cartProduct->name !!}
-            </a>
-            <p class="size">Qty: {{ $cartProduct->qty }}</p>
-
-            <p class="size">{{ @$cartProduct->options->product_size['name'] }}
-                {{ @$cartProduct->options->product_size['price'] ? '(' . currencyPosition(@$cartProduct->options->product_size['price']) . ')' : '' }}
-            </p>
-
-            @foreach ($cartProduct->options->product_options as $cartProductOption)
-                <span class="extra">{{ $cartProductOption['name'] }}
-                    ({{ currencyPosition($cartProductOption['price']) }})</span>
-            @endforeach
-            <p class="price">{{ currencyPosition($cartProduct->price) }}</p>
-        </div>
-        <span class="del_icon" onclick="removeProductFromSidebar('{{ $cartProduct->rowId }}')"><i class="fal fa-times"></i></span>
-    </li>
->>>>>>> parent of 5d5f89f9 (s)
 @endforeach
