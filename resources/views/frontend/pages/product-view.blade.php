d5e65b825987bc94de917f38aa44fbd77e54d7fd
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-07-04 13:27:03 +0530

    Update product-view.blade.php

--- resources/views/frontend/pages/product-view.blade.php
+++ resources/views/frontend/pages/product-view.blade.php
@@ -367,7 +367,7 @@
                         <h2>Related Item</h2>
                         <div class="row related_product_slider">
                             @foreach ($relatedProducts as $relatedProduct)
-                                <div class="col-lg-4 d-flex align-items-stretch wow fadeInUp" data-wow-duration="1s">
+                                <div class="col-lg-4 wow fadeInUp d-flex" data-wow-duration="1s">
                                     <div class="fp__menu_item d-flex flex-column">
                                         <div class="fp__menu_item_img">
                                             <img src="{{ asset($relatedProduct->thumb_image) }}"
@@ -375,30 +375,29 @@
                                         </div>
                                         <a class="px-2 py-1 category bg-light fw-semibold"
                                             href="#">{{ @$relatedProduct->category->name }}</a>
-                                        <div class="fp__menu_item_text mt-auto">
-                                            <p class="rating">
+                                        <div class="fp__menu_item_text flex-grow-1 d-flex flex-column">
+                                            <div class="flex-grow-1">
                                                 @if ($relatedProduct->reviews_avg_rating)
                                                     <p class="rating">
                                                         @for ($i = 1; $i <= $relatedProduct->reviews_avg_rating; $i++)
                                                             <i class="fas fa-star"></i>
                                                         @endfor
-
                                                         <span>({{ $relatedProduct->reviews_count }})</span>
                                                     </p>
                                                 @endif
-                                            </p>
-                                            <a class="my-3 title"
-                                                href="{{ route('product.show', $relatedProduct->slug) }}">{!! $relatedProduct->name !!}</a>
-                                            <p
-                                                class="top-0 px-3 py-1 text-center price fw-semibold position-absolute rounded-pill color-light-gray">
-                                                @if ($relatedProduct->offer_price > 0)
-                                                    {{ currencyPosition($relatedProduct->offer_price) }}
-                                                    <del>{{ currencyPosition($relatedProduct->price) }}</del>
-                                                @else
-                                                    {{ currencyPosition($relatedProduct->price) }}
-                                                @endif
-                                            </p>
-                                            <ul class="flex-wrap d-flex justify-content-center mt-3">
+                                                <a class="my-3 title"
+                                                    href="{{ route('product.show', $relatedProduct->slug) }}">{!! $relatedProduct->name !!}</a>
+                                                <p
+                                                    class="top-0 px-3 py-1 text-center price fw-semibold position-absolute rounded-pill color-light-gray">
+                                                    @if ($relatedProduct->offer_price > 0)
+                                                        {{ currencyPosition($relatedProduct->offer_price) }}
+                                                        <del>{{ currencyPosition($relatedProduct->price) }}</del>
+                                                    @else
+                                                        {{ currencyPosition($relatedProduct->price) }}
+                                                    @endif
+                                                </p>
+                                            </div>
+                                            <ul class="flex-wrap d-flex justify-content-center mt-auto">
                                                 <li><a href="javascript:;"
                                                         onclick="loadProductModal('{{ $relatedProduct->id }}')"><i
                                                             class="fas fa-shopping-basket"></i></a></li>
