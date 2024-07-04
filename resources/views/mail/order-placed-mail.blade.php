9a326f4565df315b5c3a07361ddaa25847a3b59a
Rahul <rahul@drupalbuilder.com>
2024-07-04 11:18:51 +0530

    Update order-placed-mail.blade.php

--- resources/views/mail/order-placed-mail.blade.php
+++ resources/views/mail/order-placed-mail.blade.php
@@ -100,7 +100,8 @@ For what reason would it be advisable for me to think about business content? Th
                         </td>
                     </tr>
                     <tr>
-                        <td align="left" style="padding-top: 20px;">
+                        <td
+                         align="left" style="padding-top: 20px;">
                             <table cellspacing="0" cellpadding="0" border="0" width="100%">
                                 <tr>
                                     <td width="75%" align="left" bgcolor="#eeeeee" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;">
