366e75e66bce9a5fdab25504d689e6665b925963
Abhishek Rajput <blazeneon1@gmail.com>
2024-07-05 12:34:59 +0530

    d

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -95,7 +95,7 @@
                 <div class="m-0 col-md-7">
                     <div class="row">
                         <div class="col-md-auto col-6">
-                            <h6><b>Edinburgh:</b> 0131 337 9990 t</h6>
+                            <h6><b>Edinburgh:</b> 0131 337 9990 </h6>
                         </div>
                         <div class="col-md-auto col-6 text-end">
                             <h6 class="text-end text-md-start"><b>Glasgow:</b> 0141 378 0027</h6>
