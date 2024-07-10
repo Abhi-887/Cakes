6157a7a2d0f66bd6913c44f7df9b7a8807affe87
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-07-10 11:56:27 +0530

    Update product.blade.php

--- resources/views/frontend/pages/product.blade.php
+++ resources/views/frontend/pages/product.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay py-5">
-            <div class="container py-5">
+            <div class="container py-md-5 py-2">
                 <div class="fp__breadcrumb_text">
                     <h1>{{ $category->name }}</h1>
                     <ul>
