cbd4ca04700d8ed5f3950b4e23b5c7f465c1b783
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-07-10 16:32:51 +0530

    Update slider.blade.php

--- resources/views/frontend/home/components/slider.blade.php
+++ resources/views/frontend/home/components/slider.blade.php
@@ -147,11 +147,13 @@
             @endif
         @endforeach
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <button class="carousel-control-prev ms-3 ms-md-5" type="button" data-bs-target="#carouselExampleControls"
+        data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <button class="carousel-control-next me-3 me-md-5" type="button" data-bs-target="#carouselExampleControls"
+        data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
     </button>
