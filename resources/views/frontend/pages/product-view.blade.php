ced25b26f874b7bb35c44d5620f89912b2e912c8
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-07-10 13:54:52 +0530

    Update product-view.blade.php

--- resources/views/frontend/pages/product-view.blade.php
+++ resources/views/frontend/pages/product-view.blade.php
@@ -42,7 +42,7 @@
             }
         }
     </style>
-    <!--=============================--> ==============================-->
+    <!--============================= BREADCRUMB START ==============================-->
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay py-5">
             <div class="container py-md-5 py-2">
@@ -56,7 +56,7 @@
             </div>
         </div>
     </section>
-    <!--=============================BREADCRUMB END ==============================-->
+    <!--============================= BREADCRUMB END ==============================-->
     <!--============================= MENU DETAILS START ==============================-->
 
 
