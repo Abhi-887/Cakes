b19add5627601a7923f8bf62988d674ca0792af6
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-07-15 13:25:08 +0530

    Update product-view.blade.php

--- resources/views/frontend/pages/product-view.blade.php
+++ resources/views/frontend/pages/product-view.blade.php
@@ -95,11 +95,11 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-5 col-md-9 wow fadeInUp" data-wow-duration="1s">
-                    <div class="hidden exzoom w-100" id="exzoom">
-                        <div class="exzoom_img_box fp__menu_details_images w-100">
-                            <ul class='exzoom_img_ul w-100'>
-                                <li><img class="zoom img-fluid" src="{{ asset($product->thumb_image) }}" alt="product">
-                                </li>
+                    <div class="hidden exzoom" id="exzoom" style="width: auto;">
+                        <div class="exzoom_img_box fp__menu_details_images" style="width: auto;">
+                            <ul class="exzoom_img_ul" style="width: auto;">
+                                <li style="width: auto;"><img class="zoom img-fluid"
+                                        src="{{ asset($product->thumb_image) }}" alt="product"></li>
 
                                 @foreach ($product->productImages as $image)
                                     <li><img class="zoom img-fluid" src="{{ asset($image->image) }}" alt="product">
