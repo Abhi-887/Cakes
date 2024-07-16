4a4d0022a59c3035592cace9d4db1bc24eb82cbe
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-07-16 13:02:43 +0530

    Update customer-feedback.blade.php

--- resources/views/frontend/pages/customer-feedback.blade.php
+++ resources/views/frontend/pages/customer-feedback.blade.php
@@ -48,8 +48,8 @@
         }
     </style>
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                            BREADCRUMB START
-                                                                                                                                                                                                                                                                                                                                                                                                   ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                BREADCRUMB START
+                                                                                                                                                                                                                                                                                                                                                                                                       ==============================-->
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay py-5">
             <div class="container py-md-5 py-2">
@@ -64,11 +64,11 @@
         </div>
     </section>
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                            BREADCRUMB END
-                                                                                                                                                                                                                                                                                                                                                                                                                                        ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                BREADCRUMB END
+                                                                                                                                                                                                                                                                                                                                                                                                                                            ==============================-->
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                           Customer-Feedback PAGE START
-                                                                                                                                                                                                                                                                                                                                                                                                                                        ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                               Customer-Feedback PAGE START
+                                                                                                                                                                                                                                                                                                                                                                                                                                            ==============================-->
 
 
     <div class="container my-5">
@@ -179,6 +179,6 @@
         </div>
     </div>
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                            Customer-Feedback  PAGE END
-                                                                                                                                                                                                                                                                                                                                                                                                                                        ==============================-->
+            Customer-Feedback  PAGE END
+        ==============================-->
 @endsection
