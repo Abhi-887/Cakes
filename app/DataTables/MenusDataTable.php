65a588e285ef2e504b1a395195f9e6fd57a0a575
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-07-18 17:34:37 +0530

    Update MenusDataTable.php

--- app/DataTables/MenusDataTable.php
+++ app/DataTables/MenusDataTable.php
@@ -86,7 +86,7 @@ class MenusDataTable extends DataTable
 
             Column::make('id'),
             Column::make('name'),
-            Column::make('parent'),
+            Column::make('parentmenus'),
             Column::make('link'),
             Column::make('status'),
             Column::computed('action')
