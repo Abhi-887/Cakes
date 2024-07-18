4ea408fed0fd89dc7b9b678bf7185fca29bf4049
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-07-18 11:34:55 +0530

    a

---
+++ resources/views/admin/menu/create.blade.php
@@ -0,0 +1,48 @@
+@extends('admin.layouts.master')
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Menus</h1>
+        </div>
+
+        <div class="card card-primary">
+            <div class="card-header">
+                <h4>Create Menus</h4>
+
+            </div>
+            <div class="card-body">
+                <form action="{{ route('admin.menus.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" name="name" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Link</label>
+                        <input type="text" name="link" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="parent">Parent</label>
+                        <select name="parent" class="form-control" id="parent">
+                            <option value="" selected>Select Parent Category</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Status</label>
+                        <select name="status" class="form-control" id="">
+                            <option value="1">Active</option>
+                            <option value="0">Inactive</option>
+                        </select>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Create</button>
+                </form>
+            </div>
+        </div>
+    </section>
+@endsection
