d98f1ca920e2bca07d161617eb30bd0675c61459
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-07-19 13:02:25 +0530

    Update cart-view.blade.php

--- resources/views/frontend/pages/cart-view.blade.php
+++ resources/views/frontend/pages/cart-view.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <!--=============================
-                                                                BREADCRUMB START
-                                                            ==============================-->
+                                                                    BREADCRUMB START
+                                                                ==============================-->
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay py-5">
             <div class="container py-md-5 py-2">
@@ -18,12 +18,12 @@
         </div>
     </section>
     <!--=============================
-                                                                BREADCRUMB END
-                                                            ==============================-->
+                                                                    BREADCRUMB END
+                                                                ==============================-->
 
     <!--============================
-                                                                CART VIEW START
-                                                            ==============================-->
+                                                                    CART VIEW START
+                                                                ==============================-->
     <section class="fp__cart_view mt_125 xs_mt_95 mb_100 xs_mb_70">
         <div class="container">
             <div class="row">
@@ -58,7 +58,7 @@
                                             </td>
 
                                             <td class="fp__pro_name">
-                                                <a class="fw-normal"
+                                                <a class="fw-bold"
                                                     href="{{ route('product.show', $product->options->product_info['slug']) }}">{{ $product->name }}</a>
                                                 <span>{{ @$product->options->product_size['name'] }}
                                                     {{ @$product->options->product_size['price']
@@ -165,8 +165,8 @@
         </div>
     </section>
     <!--============================
-                                                                CART VIEW END
-                                                            ==============================-->
+                                                                    CART VIEW END
+                                                                ==============================-->
 @endsection
 
 @push('scripts')
