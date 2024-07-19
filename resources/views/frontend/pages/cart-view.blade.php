da26805ccfe4d134938ed55daa4da839e577533e
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-07-19 12:50:02 +0530

    a

--- resources/views/frontend/pages/cart-view.blade.php
+++ resources/views/frontend/pages/cart-view.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <!--=============================
-                                                        BREADCRUMB START
-                                                    ==============================-->
+                                                                BREADCRUMB START
+                                                            ==============================-->
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay py-5">
             <div class="container py-md-5 py-2">
@@ -18,12 +18,12 @@
         </div>
     </section>
     <!--=============================
-                                                        BREADCRUMB END
-                                                    ==============================-->
+                                                                BREADCRUMB END
+                                                            ==============================-->
 
     <!--============================
-                                                        CART VIEW START
-                                                    ==============================-->
+                                                                CART VIEW START
+                                                            ==============================-->
     <section class="fp__cart_view mt_125 xs_mt_95 mb_100 xs_mb_70">
         <div class="container">
             <div class="row">
@@ -58,7 +58,7 @@
                                             </td>
 
                                             <td class="fp__pro_name">
-                                                <a class="text-secondary fw-semibold"
+                                                <a class="fw-normal"
                                                     href="{{ route('product.show', $product->options->product_info['slug']) }}">{{ $product->name }}</a>
                                                 <span>{{ @$product->options->product_size['name'] }}
                                                     {{ @$product->options->product_size['price']
@@ -67,12 +67,12 @@
                                                 </span>
                                                 <small>
                                                     @foreach ($product->options->product_options as $option)
-                                                        <p class="text-secondary fw-semibold">{{ $option['name'] }}
+                                                        <p class="text-secondary fw-normal">{{ $option['name'] }}
                                                             ({{ currencyPosition($option['price']) }})
                                                         </p>
                                                     @endforeach
                                                     @foreach ($product->options->product_variants as $variant)
-                                                        <p class="text-secondary fw-semibold">
+                                                        <p class="text-secondary fw-normal">
                                                             {{ $variant['variant_name'] }}:
                                                             {{ $variant['item_name'] }}
                                                             ({{ currencyPosition($variant['item_price']) }})
@@ -165,8 +165,8 @@
         </div>
     </section>
     <!--============================
-                                                        CART VIEW END
-                                                    ==============================-->
+                                                                CART VIEW END
+                                                            ==============================-->
 @endsection
 
 @push('scripts')
