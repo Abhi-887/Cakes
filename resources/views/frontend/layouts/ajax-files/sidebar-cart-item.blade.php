aac9236c7c120db5fc60d746dacc7ce1d9030baa
Abhishek Rajput <blazeneon1@gmail.com>
2024-07-20 17:35:06 +0530

    Update sidebar-cart-item.blade.php

--- resources/views/frontend/layouts/ajax-files/sidebar-cart-item.blade.php
+++ resources/views/frontend/layouts/ajax-files/sidebar-cart-item.blade.php
@@ -7,36 +7,52 @@ $totalPrice = $cartProduct->price; // Base product price
 
 // Add the prices of the variants to the total price
 foreach ($cartProduct->options->product_variants as $cartProductVariant) {
-$totalPrice += $cartProductVariant['item_price'];
+$totalPrice += $cartProductVariant['item_price'] ?? 0;
 }
 @endphp
 <li>
     <div class="menu_cart_img">
-        <img src="{{ asset($cartProduct->options->product_info['image']) }}" alt="menu" class="img-fluid w-100">
+        <img src="{{ asset($cartProduct->options->product_info['image'] ?? 'default-image.png') }}" alt="menu"
+            class="img-fluid w-100">
     </div>
 
     <div class="menu_cart_text">
-        <a class="title" href="{{ route('product.show', $cartProduct->options->product_info['slug']) }}">{!!
-            $cartProduct->name !!}</a>
+        <a class="title"
+            href="{{ route('product.show', $cartProduct->options->product_info['slug'] ?? 'default-slug') }}">
+            {!! htmlspecialchars($cartProduct->name) !!}
+        </a>
         <p class="size">Qty: {{ $cartProduct->qty }}</p>
 
+        {{-- Uncomment and use if needed
+        @if (!empty($cartProduct->options->product_size))
+        <p class="size">
+            {{ htmlspecialchars($cartProduct->options->product_size['name']) }}
+            {{ $cartProduct->options->product_size['price'] ? '(' .
+            currencyPosition($cartProduct->options->product_size['price']) . ')' : '' }}
+        </p>
+        @endif
+        --}}
 
-
-        {{-- @if (!empty($cartProduct->options->product_size))
-        <p class="size">{{ $cartProduct->options->product_size['name'] }} {{
-            $cartProduct->options->product_size['price'] ? '(' .
-            currencyPosition($cartProduct->options->product_size['price']) . ')' : '' }}</p>
-        @endif --}}
-
-        {{-- @foreach ($cartProduct->options->product_options as $cartProductOption)
-        <span class="extra">{{ $cartProductOption['name'] }} ({{ currencyPosition($cartProductOption['price'])
-            }})</span>
-        @endforeach --}}
+        {{-- Uncomment and use if needed
+        @foreach ($cartProduct->options->product_options as $cartProductOption)
+        <span class="extra">
+            {{ htmlspecialchars($cartProductOption['name']) }} ({{ currencyPosition($cartProductOption['price']) }})
+        </span>
+        @endforeach
+        --}}
 
         @foreach ($cartProduct->options->product_variants as $cartProductVariant)
+        @php
+        $variantName = htmlspecialchars($cartProductVariant['variant_name'] ?? '');
+        $itemName = htmlspecialchars($cartProductVariant['item_name'] ?? '');
+        $itemPrice = $cartProductVariant['item_price'] ?? 0;
+        @endphp
         <p class="variant">
-            {{ $cartProductVariant['variant_name'] }}: {{ $cartProductVariant['item_name'] }}
-            ({{ currencyPosition($cartProductVariant['item_price']) }})
+            {{ $variantName }}
+            @if (!empty($itemName))
+            : {{ $itemName }}
+            @endif
+            ({{ currencyPosition($itemPrice) }})
         </p>
         @endforeach
 
