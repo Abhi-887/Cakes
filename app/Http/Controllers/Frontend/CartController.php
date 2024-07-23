ed4ee063000004ab98df92ac8eb426b6484d8c37
Abhishek Rajput <blazeneon1@gmail.com>
2024-07-23 11:46:37 +0530

    Update CartController.php

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -94,6 +94,102 @@ class CartController extends Controller
             return response(['status' => 'error', 'message' => 'Something went wrong!'], 500);
         }
     }
+    public function addToCart(Request $request)
+    {
+        $product = Product::with(['productSizes', 'productOptions', 'variants.productVariantItems'])->findOrFail($request->product_id);
+        if ($product->quantity < $request->quantity) {
+            throw ValidationException::withMessages(['Quantity is not available!']);
+        }
+
+        try {
+            $productSize = $product->productSizes->where('id', $request->product_size)->first();
+            $productOptions = $product->productOptions->whereIn('id', $request->product_option);
+            $variantItems = $request->input('variants_items', []);
+
+            $options = [
+                'product_size' => [],
+                'product_options' => [],
+                'product_variants' => [],
+                'product_info' => [
+                    'image' => $product->thumb_image,
+                    'slug' => $product->slug
+                ]
+            ];
+
+            if ($productSize !== null) {
+                $options['product_size'] = [
+                    'id' => $productSize?->id,
+                    'name' => $productSize?->name,
+                    'price' => $productSize?->price
+                ];
+            }
+
+            foreach ($productOptions as $option) {
+                $options['product_options'][] = [
+                    'id' => $option->id,
+                    'name' => $option->name,
+                    'price' => $option->price
+                ];
+            }
+
+            foreach ($variantItems as $variantId => $itemValues) {
+                if (is_array($itemValues)) {
+                    foreach ($itemValues as $itemValue) {
+                        // Check if the item is a variant item
+                        $variantItem = $product->variants->flatMap->productVariantItems->where('id', $itemValue)->first();
+                        if ($variantItem) {
+                            $options['product_variants'][] = [
+                                'variant_id' => $variantItem->productVariant->id,
+                                'variant_name' => $variantItem->productVariant->name,
+                                'item_id' => $variantItem->id,
+                                'item_name' => $variantItem->name,
+                                'item_price' => $variantItem->price
+                            ];
+                        }
+                    }
+                } else {
+                    // Check if the item is a variant item
+                    $variantItem = $product->variants->flatMap->productVariantItems->where('id', $itemValues)->first();
+                    if ($variantItem) {
+                        $options['product_variants'][] = [
+                            'variant_id' => $variantItem->productVariant->id,
+                            'variant_name' => $variantItem->productVariant->name,
+                            'item_id' => $variantItem->id,
+                            'item_name' => $variantItem->name,
+                            'item_price' => $variantItem->price
+                        ];
+                    } else {
+                        // For custom fields, use the variant name and value
+                        $variant = $product->variants->where('id', $variantId)->first();
+                        if ($variant) {
+                            $options['product_variants'][] = [
+                                'variant_id' => $variantId,
+                                'variant_name' => $variant->name,
+                                'item_id' => null,
+                                'item_name' => $itemValues,
+                                'item_price' => null
+                            ];
+                        }
+                    }
+                }
+            }
+
+            Cart::add([
+                'id' => $product->id,
+                'name' => $product->name,
+                'qty' => $request->quantity,
+                'price' => $product->offer_price > 0 ? $product->offer_price : $product->price,
+                'weight' => 0,
+                'options' => $options
+            ]);
+
+            return response(['status' => 'success', 'message' => 'Product added into cart!'], 200);
+        } catch (\Exception $e) {
+            logger($e);
+            return response(['status' => 'error', 'message' => 'Something went wrong!'], 500);
+        }
+    }
+
 
 
     public function getCartProduct()
