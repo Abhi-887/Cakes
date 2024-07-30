4900ddde33912810ecb5e26e2175473bdb7f19c2
Abhishek Rajput <blazeneon1@gmail.com>
2024-07-30 14:15:48 +0530

    Update slider.blade.php

--- resources/views/frontend/home/components/slider.blade.php
+++ resources/views/frontend/home/components/slider.blade.php
@@ -1,22 +1,22 @@
 @php
-    $selectedOption = null;
+$selectedOption = null;
 @endphp
 
 
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
         @foreach ($sliders as $key => $slider)
-            @if ($slider->show_at_home == $selectedOption || $selectedOption === null || $selectedOption === '')
-                <div class="carousel-item{{ $key === 0 ? ' active' : '' }} background{{ $key + 1 }}">
-                    <div class="container width_Global">
-                        <div class="row h-100 align-items-center">
-                            <img src="{{ asset($slider->image) }}" class="d-block img-fluid front_image" alt="">
-                            <h2>{{ $slider->title }}</h2>
-                            <a class="mt-4" href="{{ $slider->button_link }}">{{ $slider->button_link }}</a>
-                        </div>
-                    </div>
+        @if ($slider->show_at_home == $selectedOption || $selectedOption === null || $selectedOption === '')
+        <div class="carousel-item{{ $key === 0 ? ' active' : '' }} background{{ $key + 1 }}">
+            <div class="container width_Global">
+                <div class="row h-100 align-items-center">
+                    <img src="{{ asset($slider->image) }}" class="d-block img-fluid front_image" alt="">
+                    {{-- <h2>{{ $slider->title }}</h2> --}}
+                    <a class="mt-4" href="{{ $slider->button_link }}">{{ $slider->button_link }}</a>
                 </div>
-            @endif
+            </div>
+        </div>
+        @endif
         @endforeach
     </div>
     <button class="carousel-control-prev ms-3 ms-md-5" type="button" data-bs-target="#carouselExampleControls"
@@ -59,20 +59,24 @@
     @endforeach
     }
 
-    */ @foreach ($sliders as $key => $slider)
-        .background{{ $key + 1 }} {
-            background-image: url("{{ asset($slider->background_image) }}");
-            background-color: rgba(0, 0, 0, 0);
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-position: center center;
-            position: relative;
-            height: 645px !important;
+    */
+    @foreach ($sliders as $key => $slider) .background {
+            {
+            $key +1
         }
-    @endforeach
+    }
 
+        {
+        background-image: url("{{ asset($slider->background_image) }}");
+        background-color: rgba(0, 0, 0, 0);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center center;
+        position: relative;
+        height: 645px !important;
+    }
 
-    .slider-img {
+    @endforeach .slider-img {
         position: relative;
     }
 
@@ -130,7 +134,7 @@
         z-index: 100;
         cursor: pointer;
         position: relative;
-        background: url('{{ asset('uploads/arrow4.png') }}') no-repeat 0 0 !important;
+        background: url('{{ asset(' uploads/arrow4.png') }}') no-repeat 0 0 !important;
         width: 60px;
         height: 60px;
         /* left: 20px !important; */
@@ -140,17 +144,17 @@
         z-index: 100;
         cursor: pointer;
         position: relative;
-        background: url('{{ asset('uploads/arrow3.png') }}') no-repeat 0 0 !important;
+        background: url('{{ asset(' uploads/arrow3.png') }}') no-repeat 0 0 !important;
         width: 60px;
         height: 60px;
         /* left: 20px !important; */
     }
 
     .carousel-control-prev-icon:hover {
-        background: url('{{ asset('uploads/arrow1.png') }}') no-repeat 0 0 !important;
+        background: url('{{ asset(' uploads/arrow1.png') }}') no-repeat 0 0 !important;
     }
 
     .carousel-control-next-icon:hover {
-        background: url('{{ asset('uploads/arrow2.png') }}') no-repeat 0 0 !important;
+        background: url('{{ asset(' uploads/arrow2.png') }}') no-repeat 0 0 !important;
     }
 </style>
