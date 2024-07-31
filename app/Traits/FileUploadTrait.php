da35ad205ddab99a1ba2e40ace4efa77b81211e1
Abhishek Rajput <blazeneon1@gmail.com>
2024-07-31 18:24:21 +0530

    Revert "Update FileUploadTrait.php"
    This reverts commit d63df102cc5a5aa0521c95440a3662b4ed18b370.

--- app/Traits/FileUploadTrait.php
+++ app/Traits/FileUploadTrait.php
@@ -20,10 +20,10 @@ trait FileUploadTrait
             $manager = new ImageManager(array('driver' => 'gd'));
 
             // Read image from uploaded file
-            $image = $manager->make($imageFile->getPathname());
+            $image = $manager->read($imageFile->getPathname());
 
-            // Compress image
-            $image->encode($ext, 75); // 75 is the quality percentage
+            // Resize image proportionally to 800px width
+            $image->scale(width: 300);
 
             // Save modified image to public path
             $image->save(public_path($path . '/' . $imageName));
