3d9a295f89bc4e3ce518f6bc7ba895595398d98d
Abhishek Rajput <blazeneon1@gmail.com>
2024-07-31 12:36:06 +0530

    image intervation for 300width added

--- app/Traits/ImageUploadTrait.php
+++ app/Traits/ImageUploadTrait.php
@@ -4,71 +4,102 @@ namespace App\Traits;
 
 use Illuminate\Http\Request;
 use File;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
-trait ImageUploadTrait {
-
-
+trait ImageUploadTrait
+{
     public function uploadImage(Request $request, $inputName, $path)
     {
-        if($request->hasFile($inputName)){
+        if ($request->hasFile($inputName)) {
+            $imageFile = $request->file($inputName);
+            $ext = $imageFile->getClientOriginalExtension();
+            $imageName = 'media_' . uniqid() . '.' . $ext;
 
-            $image = $request->{$inputName};
-            $ext = $image->getClientOriginalExtension();
-            $imageName = 'media_'.uniqid().'.'.$ext;
+            // Create image manager with GD driver
+            $manager = new ImageManager(new Driver());
 
-            $image->move(public_path($path), $imageName);
+            // Read image from uploaded file
+            $image = $manager->read($imageFile->getPathname());
 
-           return $path.'/'.$imageName;
-       }
+            // Resize image proportionally to 300px width
+            $image->scale(width: 300);
+
+            // Save modified image to public path
+            $image->save(public_path($path . '/' . $imageName));
+
+            return $path . '/' . $imageName;
+        }
+
+        return NULL;
     }
-
 
     public function uploadMultiImage(Request $request, $inputName, $path)
     {
         $imagePaths = [];
-        
-        if($request->hasFile($inputName)){
 
-            $images = $request->{$inputName};
+        if ($request->hasFile($inputName)) {
+            $images = $request->file($inputName);
 
-            foreach($images as $image){
+            foreach ($images as $imageFile) {
+                $ext = $imageFile->getClientOriginalExtension();
+                $imageName = 'media_' . uniqid() . '.' . $ext;
 
-                $ext = $image->getClientOriginalExtension();
-                $imageName = 'media_'.uniqid().'.'.$ext;
+                // Create image manager with GD driver
+                $manager = new ImageManager(new Driver());
 
-                $image->move(public_path($path), $imageName);
+                // Read image from uploaded file
+                $image = $manager->read($imageFile->getPathname());
 
-                $imagePaths[] =  $path.'/'.$imageName;
+                // Resize image proportionally to 300px width
+                $image->scale(width: 300);
+
+                // Save modified image to public path
+                $image->save(public_path($path . '/' . $imageName));
+
+                $imagePaths[] = $path . '/' . $imageName;
             }
 
             return $imagePaths;
-       }
+        }
+
+        return [];
     }
 
-
-    public function updateImage(Request $request, $inputName, $path, $oldPath=null)
+    public function updateImage(Request $request, $inputName, $path, $oldPath = null)
     {
-        if($request->hasFile($inputName)){
-            if(File::exists(public_path($oldPath))){
+        if ($request->hasFile($inputName)) {
+            if (File::exists(public_path($oldPath))) {
                 File::delete(public_path($oldPath));
             }
 
-            $image = $request->{$inputName};
-            $ext = $image->getClientOriginalExtension();
-            $imageName = 'media_'.uniqid().'.'.$ext;
+            $imageFile = $request->file($inputName);
+            $ext = $imageFile->getClientOriginalExtension();
+            $imageName = 'media_' . uniqid() . '.' . $ext;
 
-            $image->move(public_path($path), $imageName);
+            // Create image manager with GD driver
+            $manager = new ImageManager(new Driver());
 
-           return $path.'/'.$imageName;
-       }
+            // Read image from uploaded file
+            $image = $manager->read($imageFile->getPathname());
+
+            // Resize image proportionally to 300px width
+            $image->scale(width: 300);
+
+            // Save modified image to public path
+            $image->save(public_path($path . '/' . $imageName));
+
+            return $path . '/' . $imageName;
+        }
+
+        return NULL;
     }
 
-    /** Handle Delte File */
+    /** Handle Delete File */
     public function deleteImage(string $path)
     {
-        if(File::exists(public_path($path))){
+        if (File::exists(public_path($path))) {
             File::delete(public_path($path));
         }
     }
 }
-
