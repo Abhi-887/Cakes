c5c746f210d5de142397a7187430b0cbab5433ee
Abhishek Rajput <blazeneon1@gmail.com>
2024-08-02 12:05:22 +0530

    Update master.blade.php

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -101,8 +101,8 @@
                         <div class="text-center col-md-auto col-12">
                             <h6><b>Edinburgh:</b> 0131 337 9990 </h6>
                         </div>
-                        <div class="text-center col-md-auto col-12 text-start">
-                            <h6 class="text-start text-sm-end text-md-start"><b>Glasgow:</b> 0141 378 0027</h6>
+                        <div class="text-center col-md-auto col-12 ">
+                            <h6><b>Glasgow:</b> 0141 378 0027</h6>
                         </div>
                     </div>
                 </div>
