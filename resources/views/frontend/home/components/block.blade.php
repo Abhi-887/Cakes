be1c16829b6ffbac4f6df91c4787ee60f7aa8ee2
Abhishek Rajput <blazeneon1@gmail.com>
2024-08-02 18:07:16 +0530

    Revert "Update block.blade.php"
    This reverts commit 13bdd9b497f5e6fe23a7e947fa9f621ead80b1db.

--- resources/views/frontend/home/components/block.blade.php
+++ resources/views/frontend/home/components/block.blade.php
@@ -1,24 +1,25 @@
 @php
-$selectedOption = $selectedOption ?? null;
+    $selectedOption = $selectedOption ?? null;
 @endphp
 
 <section class="py-5">
     <div class="container">
         <div class="row">
             @foreach ($sliders2 as $slider)
-            @if ($slider->show_at_home == $selectedOption || is_null($selectedOption) || $selectedOption === '')
-            <div class=" col-4 my-3">
-                <a class="w-100" href="{{ $slider->button_link }}">
-                    <div class="image-block position-relative overflow-hidden rounded-3">
-                        <img src="{{ asset($slider->image) }}" class="img-fluid w-100 h-100" alt="{{ $slider->title }}">
-                        <div
-                            class="overlay image-textblock position-absolute w-100 h-100 d-flex top-0 start-0 justify-content-center align-items-center">
-                            <h3 class="overlay-text text-white text-center">{{ $slider->title }}</h3>
-                        </div>
+                @if ($slider->show_at_home == $selectedOption || is_null($selectedOption) || $selectedOption === '')
+                    <div class="col-12 col-md-6 col-lg-4 my-3">
+                        <a class="w-100" href="{{ $slider->button_link }}">
+                            <div class="image-block position-relative overflow-hidden rounded-3">
+                                <img src="{{ asset($slider->image) }}" class="img-fluid w-100 h-100"
+                                    alt="{{ $slider->title }}">
+                                <div
+                                    class="overlay image-textblock position-absolute w-100 h-100 d-flex top-0 start-0 justify-content-center align-items-center">
+                                    <h3 class="overlay-text text-white text-center">{{ $slider->title }}</h3>
+                                </div>
+                            </div>
+                        </a>
                     </div>
-                </a>
-            </div>
-            @endif
+                @endif
             @endforeach
         </div>
     </div>
