321cf579b7b33df05c1f1948c54bc3e41ee2278c
Abhishek Rajput <blazeneon1@gmail.com>
2024-08-12 12:49:55 +0530

    Update about.blade.php

--- resources/views/frontend/pages/about.blade.php
+++ resources/views/frontend/pages/about.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <!--=============================
-                BREADCRUMB START
-            ==============================-->
+                            BREADCRUMB START
+                        ==============================-->
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay py-5">
             <div class="container py-md-5 py-2">
@@ -18,15 +18,15 @@
         </div>
     </section>
     <!--=============================
-                BREADCRUMB END
-            ==============================-->
+                            BREADCRUMB END
+                        ==============================-->
 
 
     <!--=============================
-                ABOUT PAGE START
-            ============================== -->
+                            ABOUT PAGE START
+                        ============================== -->
     <section class="fp__about_us mt_120 xs_mt_90">
-        <div class="container">
+        <div class="container custom-container ">
             <div class="row">
                 <div class="col-xl-6 col-lg-7 wow fadeInUp" data-wow-duration="1s">
                     <div class="fp__section_heading mb_40">
@@ -66,12 +66,12 @@
     </section>
 
     <!--=============================
-            WHY CHOOSE START
-        ==============================-->
+                        WHY CHOOSE START
+                    ==============================-->
     <!-- @include('frontend.home.components.why-choose') -->
     <!--=============================
-            WHY CHOOSE END
-        ==============================-->
+                        WHY CHOOSE END
+                    ==============================-->
 
     <section class="fp__about_video mt_100 xs_mt_70 my-5">
         <div class="container wow fadeInUp" data-wow-duration="1s">
@@ -94,28 +94,28 @@
     </section>
 
     <!--=============================
-            TEAM START
-            ==============================-->
+                        TEAM START
+                        ==============================-->
     <!--  @include('frontend.home.components.team')-->
     <!--=============================
-            TEAM END
-            ==============================-->
+                        TEAM END
+                        ==============================-->
 
     <!--=============================
-                COUNTER START
-            ==============================-->
+                            COUNTER START
+                        ==============================-->
     <!--  @include('frontend.home.components.counter')-->
     <!--=============================
-                    COUNTER END
-            ==============================-->
+                                COUNTER END
+                        ==============================-->
     <!--=============================
-               TESTIMONIAL  START
-            ==============================-->
+                           TESTIMONIAL  START
+                        ==============================-->
     <!--  @include('frontend.home.components.testimonial')-->
     <!--=============================
-                    TESTIMONIAL END
-            ==============================-->
+                                TESTIMONIAL END
+                        ==============================-->
     <!--=============================
-                ABOUT PAGE END
-            ==============================-->
+                            ABOUT PAGE END
+                        ==============================-->
 @endsection
