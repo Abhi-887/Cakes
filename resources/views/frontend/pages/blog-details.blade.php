3b568793944868c21a2a503aead3820b9ce52527
Abhishek Rajput <blazeneon1@gmail.com>
2024-08-12 11:32:15 +0530

    Update blog-details.blade.php

--- resources/views/frontend/pages/blog-details.blade.php
+++ resources/views/frontend/pages/blog-details.blade.php
@@ -11,8 +11,8 @@
 
 @section('content')
     <!--=============================
-                                        BREADCRUMB START
-                                    ==============================-->
+                                                    BREADCRUMB START
+                                                ==============================-->
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay py-5">
             <div class="container py-md-5 py-2">
@@ -27,13 +27,13 @@
         </div>
     </section>
     <!--=============================
-                                        BREADCRUMB END
-                                    ==============================-->
+                                                    BREADCRUMB END
+                                                ==============================-->
 
 
     <!--=========================
-                                        BLOG DETAILS START
-                                    ==========================-->
+                                                    BLOG DETAILS START
+                                                ==========================-->
     <section class="fp__blog_details mt_120 xs_mt_90 mb_100 xs_mb_70">
         <div class="container">
             <div class="row">
@@ -45,7 +45,7 @@
                         <div class="fp__blog_details_text wow fadeInUp" data-wow-duration="1s">
                             <ul class="details_bloger d-flex flex-wrap">
                                 <li><i class="far fa-user"></i> By {{ $blog->user->name }}</li>
-                                <li><i class="far fa-comment-alt-lines"></i> 12 Comments</li>
+                                <li><i class="far fa-comment-alt-lines"></i> {{ $blog->comments_count }} Comments</li>
                                 <li><i class="far fa-calendar-alt"></i> {{ date('d m Y', strtotime($blog->created_at)) }}
                                 </li>
                             </ul>
@@ -186,6 +186,6 @@
         </div>
     </section>
     <!--=========================
-                                        BLOG DETAILS END
-                                    ==========================-->
+                                                    BLOG DETAILS END
+                                                ==========================-->
 @endsection
