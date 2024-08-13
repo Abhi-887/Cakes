fbe4ad50fa93cc11d09f93f8148cf83b97c20e6e
Abhishek Rajput <blazeneon1@gmail.com>
2024-08-13 11:45:54 +0530

    Update WishlistController.php

--- app/Http/Controllers/Frontend/WishlistController.php
+++ app/Http/Controllers/Frontend/WishlistController.php
@@ -1,44 +1,32 @@
 <?php
+
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Wishlist;
+use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 
 class WishlistController extends Controller
 {
-    public function store(string $productId): Response
+    function store(string $productId): Response
     {
-        // Check if the user is logged in
-        if (!Auth::check()) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Please log in to add products to your wishlist.',
-                'redirect' => route('login') // Redirect URL to the login page
-            ], 401); // Unauthorized status code
-        }
 
-        // Check if the product is already in the user's wishlist
-        $productAlreadyExist = Wishlist::where('user_id', Auth::id())->where('product_id', $productId)->exists();
+        $productAlreadyExist = Wishlist::where(['user_id' => auth()->user()->id, 'product_id' => $productId])->exists();
         if ($productAlreadyExist) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Product is already in your wishlist.'
-            ], 422); // Unprocessable Entity status code
+            throw ValidationException::withMessages(['Product is already add to wishlist ']);
+        }
+        if (!Auth::check()) {
+            throw ValidationException::withMessages(['Please login for add product in wishlist']);
         }
 
-        // Add the product to the wishlist
         $wishlist = new Wishlist();
-        $wishlist->user_id = Auth::id();
+        $wishlist->user_id = auth()->user()->id;
         $wishlist->product_id = $productId;
         $wishlist->save();
 
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Product added to your wishlist!'
-        ], 200); // OK status code
+        return response(['status' => 'success', 'message' => 'Product added to wishlist!']);
     }
 }
