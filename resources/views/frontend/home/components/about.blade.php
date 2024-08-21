0489b39f43da6653f4179103fb235f2826a7d96d
Abhishek Rajput <blazeneon1@gmail.com>
2024-08-21 15:31:17 +0530

    Update about.blade.php

--- resources/views/frontend/home/components/about.blade.php
+++ resources/views/frontend/home/components/about.blade.php
@@ -1,15 +1,10 @@
 <section>
     <div id="parallax" class="py-5 bg-cover parallax-item w-100">
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="text-center col-lg-10">
-                    <h1 class="fw-bold text-light display-5">{{ $aboutus->title }}
-                        <span class="cakecolor fw-semibold display-6 ms-3">3D Cakes</span>
-                    </h1>
-                    <div class="mt-3 about-description">
-                        {!! $aboutus->description !!}
-                    </div>
-                </div>
+            <div class="text-center">
+                <h1 class="fw-bold text-light display-5">{{ $aboutus->title }}<span
+                        class="cakecolor fw-semibold display-6 ms-3">3D Cakes</span></h1>
+                <div class="mt-3 about-description">{!! $aboutus->description !!}</div>
             </div>
         </div>
     </div>
