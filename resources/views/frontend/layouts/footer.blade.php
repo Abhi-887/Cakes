980b4d677f3545530eba44bb0ef3861e49d5dc75
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-08-22 18:39:30 +0530

    Update footer.blade.php

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -43,10 +43,10 @@
                 <div class="col-lg-3 col-md-12 footer-info">
                     <h5 class="mt-2 mb-1 text-white mb-xl-3 fw-normal mt-xl-0">{{ @$footerInfo->name }}</h5>
                     <div class="">{!! @$footerInfo->address !!}</div>
-                    <div><strong class=" fas fa-solid fa-envelope"> </strong><span
+                    <div><strong class=" fas fa-solid fa-envelope">&nbsp; &nbsp; </strong><span
                             class="text-white ">{{ @$footerInfo->email }}</span>
                     </div>
-                    <div><strong class="rotate-90 fas fa-solid fa-phone"> </strong><span
+                    <div><strong class="rotate-90 fas fa-solid fa-phone">&nbsp; &nbsp; </strong><span
                             class="text-white">{{ @$footerInfo->phone }}</span>
                     </div>
                 </div>
@@ -54,7 +54,8 @@
                 <div class="col-lg-3 col-md-12 footer-info">
                     <h5 class="mt-2 mb-1 text-white mb-xl-3 fw-normal mt-xl-0">{{ @$footerInfoTwo->name }}</h5>
                     <div class="">{!! @$footerInfoTwo->address !!}</div>
-                    <div><strong class=" fas fa-solid fa-envelope"> </strong>{{ @$footerInfoTwo->email }}</div>
+                    <div><strong class=" fas fa-solid fa-envelope">&nbsp; &nbsp; </strong>{{ @$footerInfoTwo->email }}
+                    </div>
                     <div><strong class="rotate-90 fas fa-solid fa-phone"> </strong>{{ @$footerInfoTwo->phone }}</div>
                 </div>
 
