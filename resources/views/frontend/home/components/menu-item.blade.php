064274c5c57f16bff9fad9eb155f9634e3d76f47
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-08-22 15:27:47 +0530

    a

--- resources/views/frontend/home/components/menu-item.blade.php
+++ resources/views/frontend/home/components/menu-item.blade.php
@@ -124,7 +124,7 @@
 
     .category {
         position: absolute;
-        top: 66% !important;
+        top: 65% !important;
         left: 16px;
         padding: 5px 10px;
         font-size: 12px;
