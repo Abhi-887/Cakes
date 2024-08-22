a2efbc6625cbc95168e49058c92aaf3f51a1be61
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-08-22 18:07:27 +0530

    Update menu-item.blade.php

--- resources/views/frontend/home/components/menu-item.blade.php
+++ resources/views/frontend/home/components/menu-item.blade.php
@@ -236,58 +236,7 @@
         </div>
     </div>
 </div>
-<section class="bg-light pt-5 pb-5 shadow-sm">
-    <div class="container">
-        <div class="row pt-5">
-            <div class="col-12">
-                <h3 class="text-uppercase border-bottom mb-4">Equal height Bootstrap 5 cards example</h3>
-            </div>
-        </div>
-        <div class="row">
-            <!--ADD CLASSES HERE d-flex align-items-stretch-->
-            <div class="col-lg-4 mb-3 d-flex align-items-stretch">
-                <div class="card">
-                    <img src="https://i.postimg.cc/28PqLLQC/dotonburi-canal-osaka-japan-700.jpg" class="card-img-top"
-                        alt="Card Image">
-                    <div class="card-body d-flex flex-column">
-                        <h5 class="card-title">Dōtonbori Canal</h5>
-                        <p class="card-text mb-4">Is a manmade waterway dug in the early 1600's and now displays many
-                            landmark commercial locals and vivid neon signs.</p>
-                        <a href="#" class="btn btn-primary mt-auto align-self-start">Book now</a>
-                    </div>
-                </div>
-            </div>
-            <!--ADD CLASSES HERE d-flex align-items-stretch-->
-            <div class="col-lg-4 mb-3 d-flex align-items-stretch">
-                <div class="card">
-                    <img src="https://i.postimg.cc/4xVY64PV/porto-timoni-double-beach-corfu-greece-700.jpg"
-                        class="card-img-top" alt="Card Image">
-                    <div class="card-body d-flex flex-column">
-                        <h5 class="card-title">Porto Timoni Double Beach</h5>
-                        <p class="card-text mb-4">Near Afionas village, on the west coast of Corfu island. The two
-                            beaches form two unique bays. The turquoise color of the sea contrasts to the high green
-                            hills surrounding it.</p>
-                        <a href="#" class="btn btn-primary mt-auto align-self-start">Book now</a>
-                    </div>
-                </div>
-            </div>
-            <!--ADD CLASSES HERE d-flex align-items-stretch-->
-            <div class="col-lg-4 mb-3 d-flex align-items-stretch">
-                <div class="card">
-                    <img src="https://i.postimg.cc/TYyLPJWk/tritons-fountain-valletta-malta-700.jpg"
-                        class="card-img-top" alt="Card Image">
-                    <div class="card-body d-flex flex-column">
-                        <h5 class="card-title">Tritons Fountain</h5>
-                        <p class="card-text mb-4">Located just outside the City Gate of Valletta, Malta. It consists of
-                            three bronze Tritons holding up a large basin, balanced on a concentric base built out of
-                            concrete and clad in travertine slabs.</p>
-                        <a href="#" class="btn btn-primary mt-auto align-self-start">Book now</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
 <script>
