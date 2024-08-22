0e44c56f75ff5e19e41067cc09ae0a1e97b82305
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-08-22 18:28:07 +0530

    Update menu-item.blade.php

--- resources/views/frontend/home/components/menu-item.blade.php
+++ resources/views/frontend/home/components/menu-item.blade.php
@@ -197,22 +197,37 @@
                 @endphp
 
                 @foreach ($products as $product)
-                    <div class="col-md-4 my-3 d-flex align-items-stretch {{ $category->slug }}">
-                        <div class="card position-relative m-3 fp__menu_item rounded-5">
-                            <img src="{{ asset($product->thumb_image) }}" alt="{{ $product->name }}"
-                                class="img-fluid w-100">
+                    <div class="col-md-4 my-3 wow fadeInUp {{ $category->slug }}">
+                        <div class="position-relative m-3 fp__menu_item rounded-5">
+                            <div class="fp__menu_item_img">
+                                <img src="{{ asset($product->thumb_image) }}" alt="{{ $product->name }}"
+                                    class="img-fluid w-100">
+                            </div>
+
                             <a class="heart position-absolute rounded-circle" href="javascript:;"
                                 onclick="addToWishlist('{{ $product->id }}')"><i
                                     class="fal fa-heart text-white"></i></a>
 
-                            <div class="card-body d-flex flex-column fp__menu_item_text position-relative">
-
+                            <div class="fp__menu_item_text position-relative">
                                 <a class="px-3 py-2 category categorys fw-semibold"
                                     href="{{ route('category.show', ['slug' => $product->category->slug]) }}">{{ @$product->category->name }}</a>
-                                <a class="mb-5 title card-title"
+                                <a class="my-0 title"
                                     href="{{ route('product.show', $product->slug) }}">{{ $product->name }}</a>
-                                <a class="add-to-cart rounded-pill px-3 py-2 background-light-gray text-white mt-auto align-self-start"
-                                    href="javascript:;" onclick="addToCart('{{ $product->id }}')">Add to cart</a>
+                                <div class="actions mt-3 d-flex justify-content-between align-items-center">
+                                    <p class="price color-light-gray m-0">
+                                        @if ($product->offer_price > 0)
+                                            <del>{{ currencyPosition($product->price) }}</del>
+                                            {{ currencyPosition($product->offer_price) }}
+                                        @else
+                                            {{ currencyPosition($product->price) }}
+                                        @endif
+                                    </p>
+                                    {{-- <a class="eye position-absolute rounded-circle"
+                                        href="{{ route('product.show', $product->slug) }}"><i
+                                            class="far fa-eye  text-white"></i></a> --}}
+                                    <a class="add-to-cart rounded-pill px-3 py-2 background-light-gray text-white"
+                                        href="javascript:;" onclick="addToCart('{{ $product->id }}')">Add to cart</a>
+                                </div>
                             </div>
                         </div>
                     </div>
