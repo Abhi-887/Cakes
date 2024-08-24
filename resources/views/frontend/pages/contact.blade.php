7bcbdf24da5f810186718ab35afe00e6e7ac5529
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-08-24 14:19:00 +0530

    Update contact.blade.php

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                BREADCRUMB START
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    BREADCRUMB START
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ==============================-->
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay py-5">
             <div class="container py-md-5 py-2">
@@ -18,13 +18,13 @@
         </div>
     </section>
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    BREADCRUMB END
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        BREADCRUMB END
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ==============================-->
 
 
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    CONTACT PAGE START
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        CONTACT PAGE START
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ==============================-->
 
 
     <style>
@@ -66,13 +66,6 @@
 
         .rotate90 {
             rotate: 90deg;
-        }
-
-        .opening-bg {
-            background-position: center;
-            background-size: cover;
-            background-repeat: no-repeat;
-
         }
     </style>
 
@@ -137,7 +130,8 @@
         </div>
     </div>
 
-    <div class="contact-card opening-bg py-5 px-3" style="background: url({{ asset('uploads/Contact-bg.jpg') }});">
+    <div class="contact-card opening-bg py-5 px-3"
+        style="background: url({{ asset('uploads/Contact-bg.jpg') }}); background-position: center; background-size: cover; background-repeat: no-repeat;">
         <h2 class="text-center text-white fw-bold">Opening Times</h2>
         <div class="row pt-5">
             <div class="col-md-4 d-flex justify-content-center text-center text-md-start my-2">
@@ -223,7 +217,8 @@
         </div>
     </div>
 
-    <div class="contact-card opening-bg py-5 px-3" style= "background: url({{ asset('uploads/Contact-bg.jpg') }})";>
+    <div class="contact-card opening-bg py-5 px-3"
+        style= "background: url({{ asset('uploads/Contact-bg.jpg') }}); background-repeat: no-repeat; background-position: center; background-size: cover;">
         <h2 class="text-center fw-bold text-white">Opening Times</h2>
         <div class="row pt-5">
             <div class="col-md-4 d-flex justify-content-center text-center text-md-start my-2">
