067a3b8b56176429c213f122684ecfd9b5922af5
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-08-24 14:11:15 +0530

    Update contact.blade.php

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                BREADCRUMB START
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                BREADCRUMB START
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ==============================-->
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay py-5">
             <div class="container py-md-5 py-2">
@@ -18,13 +18,13 @@
         </div>
     </section>
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    BREADCRUMB END
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    BREADCRUMB END
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ==============================-->
 
 
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    CONTACT PAGE START
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    CONTACT PAGE START
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ==============================-->
 
 
     <style>
@@ -60,7 +60,7 @@
         }
 
         .contact-social-icons i:hover {
-            color: #ffffff !important;
+            color: #788a9f !important;
             transition: transform 0.6s;
         }
 
@@ -131,24 +131,24 @@
     </div>
 
     <div class="contact-card py-5 px-3" style= "background: url({{ asset('uploads/Contact-bg.jpg') }})";>
-        <h2 class="color-dark-gray text-center fw-bold">Opening Times</h2>
+        <h2 class="text-center text-white fw-bold">Opening Times</h2>
         <div class="row pt-5">
             <div class="col-md-4 d-flex justify-content-center text-center text-md-start my-2">
                 <div>
-                    <h4 class="color-light-gray mb-3"> {!! @$contact->title_one !!}</h4>
+                    <h4 class="text-white mb-3"> {!! @$contact->title_one !!}</h4>
                     <p class="fw-semibold">{!! @$contact->Description_one !!}</p>
                 </div>
             </div>
             <div class="col-md-4 d-flex justify-content-center text-center text-md-start my-2">
                 <div>
-                    <h4 class="color-light-gray mb-3">{!! @$contact->title_two !!}</h4>
-                    <p class="fw-semibold">{!! @$contact->Description_two !!}</p>
+                    <h4 class="text-white mb-3">{!! @$contact->title_two !!}</h4>
+                    <p class="fw-semibold text-white">{!! @$contact->Description_two !!}</p>
                 </div>
             </div>
             <div class="col-md-4 d-flex justify-content-center text-center text-md-start my-2">
                 <div>
-                    <h4 class="color-light-gray mb-3">{!! @$contact->title_three !!}</h4>
-                    <p class="fw-semibold">{!! @$contact->Description_three !!}</p>
+                    <h4 class="text-white mb-3">{!! @$contact->title_three !!}</h4>
+                    <p class="fw-semibold text-white">{!! @$contact->Description_three !!}</p>
                 </div>
             </div>
         </div>
@@ -217,24 +217,24 @@
     </div>
 
     <div class="contact-card py-5 px-3" style= "background: url({{ asset('uploads/Contact-bg.jpg') }})";>
-        <h2 class="color-dark-gray text-center fw-bold">Opening Times</h2>
+        <h2 class="text-center fw-bold text-white">Opening Times</h2>
         <div class="row pt-5">
             <div class="col-md-4 d-flex justify-content-center text-center text-md-start my-2">
                 <div>
-                    <h4 class="color-light-gray mb-3">{!! @$contact2->title_one !!}</h4>
-                    <p class="fw-semibold">{!! @$contact2->description_one !!}</p>
+                    <h4 class="text-white mb-3">{!! @$contact2->title_one !!}</h4>
+                    <p class="fw-semibold text-white">{!! @$contact2->description_one !!}</p>
                 </div>
             </div>
             <div class="col-md-4 d-flex justify-content-center text-center text-md-start my-2">
                 <div>
-                    <h4 class="color-light-gray mb-3">{!! @$contact2->title_two !!}</h4>
-                    <p class="fw-semibold">{!! @$contact2->description_two !!}</p>
+                    <h4 class="text-white mb-3">{!! @$contact2->title_two !!}</h4>
+                    <p class="fw-semibold text-white">{!! @$contact2->description_two !!}</p>
                 </div>
             </div>
             <div class="col-md-4 d-flex justify-content-center text-center text-md-start my-2">
                 <div>
-                    <h4 class="color-light-gray mb-3">{!! @$contact2->title_three !!}</h4>
-                    <p class="fw-semibold">{!! @$contact2->description_three !!}</p>
+                    <h4 class="text-white mb-3">{!! @$contact2->title_three !!}</h4>
+                    <p class="fw-semibold text-white">{!! @$contact2->description_three !!}</p>
                 </div>
             </div>
         </div>
