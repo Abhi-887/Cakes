2dd62145c09d9a6885f485d537a6f16574ccafa0
Abhishek Rajput <blazeneon1@gmail.com>
2024-08-29 16:45:52 +0530

    Update OrderPaymentUpdateListener.php

--- app/Listeners/OrderPaymentUpdateListener.php
+++ app/Listeners/OrderPaymentUpdateListener.php
@@ -4,6 +4,8 @@ namespace App\Listeners;
 
 use App\Events\OrderPaymentUpdateEvent;
 use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
@@ -24,11 +26,31 @@ class OrderPaymentUpdateListener
     {
         $orderId = $event->orderId;
         $order = Order::find($orderId);
+
+        // Update the payment information
         $order->payment_method = $event->paymentMethod;
         $order->payment_status = $event->paymentInfo['status'];
         $order->payment_approve_date = now();
         $order->transaction_id = $event->paymentInfo['transaction_id'];
         $order->currency_name = $event->paymentInfo['currency'];
         $order->save();
+
+        // Check if the payment status is successful
+        if ($event->paymentInfo['status'] === 'success') {
+            // Loop through the order items and decrease the product quantity
+            $orderItems = OrderItem::where('order_id', $order->id)->get();
+
+            foreach ($orderItems as $orderItem) {
+                $productModel = Product::find($orderItem->product_id);
+                if ($productModel) {
+                    $productModel->decrement('quantity', $orderItem->qty);
+
+                    // If the product quantity is now 0 or less, mark it as out of stock
+                    if ($productModel->quantity <= 0) {
+                        $productModel->update(['out_of_stock' => 1]);
+                    }
+                }
+            }
+        }
     }
 }
