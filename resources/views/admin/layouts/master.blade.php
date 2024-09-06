3bc4f3441dc860cf02b2006796541e784eaf1093
Abhishek Rajput <blazeneon1@gmail.com>
2024-09-06 17:15:23 +0530

    Update master.blade.php

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -49,7 +49,7 @@
             </div>
             <footer class="main-footer">
                 <div class="footer-left">
-                    Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Developed By DrupalBuilder Pvt. Ltd
+                    Copyright &copy; {{ date('Y') }} <div class="bullet"></div>
                 </div>
                 <div class="footer-right">
 
