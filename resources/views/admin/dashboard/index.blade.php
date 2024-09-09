88dbc14b6021b100e6ba71a9df9fa8f15b0a2dcf
Abhishek Rajput <blazeneon1@gmail.com>
2024-09-09 13:34:13 +0530

    Update index.blade.php

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -276,29 +276,31 @@
                             </div>
                             <!-- /chart-default -->
                             <!-- chart-default -->
-                            <div class="wg-chart-default">
-                                <div class="flex items-center justify-between">
-                                    <div class="flex items-center gap14">
-                                        <div class="image type-white">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="52" viewBox="0 0 48 52" fill="none">
-                                                <path d="M19.1094 2.12943C22.2034 0.343099 26.0154 0.343099 29.1094 2.12943L42.4921 9.85592C45.5861 11.6423 47.4921 14.9435 47.4921 18.5162V33.9692C47.4921 37.5418 45.5861 40.8431 42.4921 42.6294L29.1094 50.3559C26.0154 52.1423 22.2034 52.1423 19.1094 50.3559L5.72669 42.6294C2.63268 40.8431 0.726688 37.5418 0.726688 33.9692V18.5162C0.726688 14.9435 2.63268 11.6423 5.72669 9.85592L19.1094 2.12943Z" fill="#CBD5E1"/>
-                                            </svg>
-                                            <i class="icon-file"></i>
-                                        </div>
-                                        <div>
-                                            <div class="body-text mb-2">Orders Paid</div>
-                                            <h4>34,945</h4>
-                                        </div>
-                                    </div>
-                                    <div class="box-icon-trending">
-                                        <i class="icon-trending-up"></i>
-                                        <div class="body-title number">0.00%</div>
-                                    </div>
-                                </div>
-                                <div class="wrap-chart">
-                                    <div id="line-chart-3"></div>
-                                </div>
-                            </div>
+                            <!-- Chart Section -->
+<div class="wg-chart-default">
+    <div class="flex items-center justify-between">
+        <div class="flex items-center gap14">
+            <div class="image type-white">
+                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="52" viewBox="0 0 48 52" fill="none">
+                    <path d="M19.1094 2.12943C22.2034 0.343099 26.0154 0.343099 29.1094 2.12943L42.4921 9.85592C45.5861 11.6423 47.4921 14.9435 47.4921 18.5162V33.9692C47.4921 37.5418 45.5861 40.8431 42.4921 42.6294L29.1094 50.3559C26.0154 52.1423 22.2034 52.1423 19.1094 50.3559L5.72669 42.6294C2.63268 40.8431 0.726688 37.5418 0.726688 33.9692V18.5162C0.726688 14.9435 2.63268 11.6423 5.72669 9.85592L19.1094 2.12943Z" fill="#CBD5E1"/>
+                </svg>
+                <i class="icon-file"></i>
+            </div>
+            <div>
+                <div class="body-text mb-2">Orders Paid</div>
+                <h4>{{ number_format($totalOrders) }}</h4>
+            </div>
+        </div>
+        <div class="box-icon-trending">
+            <i class="icon-trending-up"></i>
+            <div class="body-title number">{{ number_format($percentageChange, 2) }}%</div>
+        </div>
+    </div>
+    <div class="wrap-chart">
+        <div id="line-chart-3"></div>
+    </div>
+</div>
+
                             <!-- /chart-default -->
                             <!-- chart-default -->
                             <div class="wg-chart-default">
@@ -1281,6 +1283,26 @@
 
 @push('scripts')
     {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+{{--
+    <script>
+        var options = {
+            chart: {
+                type: 'line',
+                height: 350
+            },
+            series: [{
+                name: 'Sales',
+                data: @json($salesData) // Pass data from controller
+            }],
+            xaxis: {
+                categories: @json($salesCategories) // Pass categories (e.g., months)
+            }
+        }
+
+        var chart = new ApexCharts(document.querySelector("#line-chart-1"), options);
+        chart.render();
+    </script> --}}
+
 
     <script>
         $(document).ready(function(){
