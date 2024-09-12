5306bd93d1b1fbc7a7082e9091f9c9aacf2e62fc
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-09-12 16:23:21 +0530

    Update menu.blade.php

--- resources/views/frontend/layouts/menu.blade.php
+++ resources/views/frontend/layouts/menu.blade.php
@@ -71,14 +71,12 @@
                     </a>
                 </li> --}}
                 <li class="nav-item">
-                    <a class="nav-link border px-3 py-2 ms-3 rounded-1" href="{{ route('login') }}"><i
-                            class="far fa-user me-3"></i>Account</a>
+                    <a class="nav-link ms-3 rounded-1" href="{{ route('login') }}"><i class="far fa-user me-3"></i></a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link border d-inline px-3 py-2 ms-3 rounded-1 position-relative transitions"
-                        href="#" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i
-                            class="far fa-heart me-3"></i>Wishlist</a>
+                    <a class="nav-link d-inline ms-3 rounded-1 position-relative transitions" href="#"
+                        data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="far fa-heart me-3"></i></a>
                 </li>
             </ul>
             <a class="d-xxl-none d-block" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button"
