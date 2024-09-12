72e8b42375d2641471720cca498b45251abb9432
drupalbuilder-shubham <133983331+drupalbuilder-shubham@users.noreply.github.com>
2024-09-12 17:20:12 +0530

    Update sidebar.blade.php

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -133,7 +133,11 @@
             </a>
         </li>
 
-
+		<li class="{{ setSidebarActive(['admin.aboutus.*']) }}">
+            <a class="nav-link" href="{{ route('admin.aboutus.index') }}"><i class="far fa-regular fa-message"></i>
+                <span>About Section</span>
+            </a>
+        </li>
 
         <li class="{{ setSidebarActive(['admin.consultation.*']) }}">
             <a class="nav-link" href="{{ route('admin.consultation.index') }}"><i
