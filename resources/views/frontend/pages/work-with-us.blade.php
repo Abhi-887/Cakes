2f1549403bb132e37c11ba63141ef78a7ca73eb4
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-09-13 15:07:07 +0530

    Update work-with-us.blade.php

--- resources/views/frontend/pages/work-with-us.blade.php
+++ resources/views/frontend/pages/work-with-us.blade.php
@@ -8,8 +8,8 @@
         }
     </style>
     <!--=============================
-                                                                                BREADCRUMB START
-                                                                            ==============================-->
+                                                                                        BREADCRUMB START
+                                                                                    ==============================-->
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay py-5">
             <div class="container py-md-5 py-2">
@@ -24,11 +24,11 @@
         </div>
     </section>
     <!--=============================
-                                                                                BREADCRUMB END
-                                                                            ==============================-->
+                                                                                        BREADCRUMB END
+                                                                                    ==============================-->
     <!--=============================
-                                                                               WE'RE HIRING! PAGE START
-                                                                            ==============================-->
+                                                                                       WE'RE HIRING! PAGE START
+                                                                                    ==============================-->
 
     {{-- <div class="container my-5">
         <div class="">
@@ -198,10 +198,10 @@
             <h3>Current vacancies are displayed below:</h3>
         </div>
 
-        <div class="hiring">
+        <div class="hiring p-5">
             <div class="row">
                 <div class="col-6 my-3">
-                    <div class="p-5">
+                    <div class="">
                         <div class="mb-3">
                             <h4>Part Time Front-of-House Assistant (Edinburgh) Wednesday - Friday</h4>
                         </div>
@@ -223,7 +223,7 @@
 
                 <div class="col-6 mb-3">
                     <div class="service-img">
-                        <img class="img-fluid" src="{{ asset('uploads/customer-service.jpeg') }}" alt="">
+                        <img class="object-fit-none" src="{{ asset('uploads/customer-service.jpeg') }}" alt="">
                     </div>
                 </div>
             </div>
@@ -366,6 +366,6 @@
         </form>
     </div>
     <!--=============================
-                                                                                 Application PAGE END
-                                                                            ==============================-->
+                                                                                         Application PAGE END
+                                                                                    ==============================-->
 @endsection
