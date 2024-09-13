a0649298ff1daf07de590d31b5758560da903e47
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-09-13 17:15:17 +0530

    Update work-with-us.blade.php

--- resources/views/frontend/pages/work-with-us.blade.php
+++ resources/views/frontend/pages/work-with-us.blade.php
@@ -8,8 +8,8 @@
         }
     </style>
     <!--=============================
-                                                                                                                                                                                                                                                    BREADCRUMB START
-                                                                                                                                                                                                                                                ==============================-->
+                                                                                                                                                                                                                                                            BREADCRUMB START
+                                                                                                                                                                                                                                                        ==============================-->
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay py-5">
             <div class="container py-md-5 py-2">
@@ -24,11 +24,11 @@
         </div>
     </section>
     <!--=============================
-                                                                                                                                                                                                                                                    BREADCRUMB END
-                                                                                                                                                                                                                                                ==============================-->
+                                                                                                                                                                                                                                                            BREADCRUMB END
+                                                                                                                                                                                                                                                        ==============================-->
     <!--=============================
-                                                                                                                                                                                                                                                   WE'RE HIRING! PAGE START
-                                                                                                                                                                                                                                                ==============================-->
+                                                                                                                                                                                                                                                           WE'RE HIRING! PAGE START
+                                                                                                                                                                                                                                                        ==============================-->
 
     {{-- <div class="container my-5">
         <div class="">
@@ -187,7 +187,7 @@
     </style>
 
 
-    <div class="my-5">
+    <div class="pt-5">
         <div class="container my-5">
             <div class="">
                 <h5>To apply to join the team at 3D Cakes Edinburgh or 3D Cakes Milngavie, please complete the application
@@ -424,6 +424,6 @@
         </div>
     </div>
     <!--=============================
-         Application PAGE END
-        ==============================-->
+                 Application PAGE END
+                ==============================-->
 @endsection
