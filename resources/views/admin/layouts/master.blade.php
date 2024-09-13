6ec7cace3fd2f88934f19339f860d7e442825d4f
Abhishek Rajput <blazeneon1@gmail.com>
2024-09-13 12:01:25 +0530

    Update master.blade.php

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -77,15 +77,15 @@
 
             <!-- Main Content -->
             <div class="main-content">
+                <div class="main-content-inner">
                 @yield('content')
+                </div>
             </div>
             <footer class="main-footer">
-                <div class="footer-left">
+                <div class="bottom-page">
                     Copyright &copy; {{ date('Y') }} <div class="bullet"></div>
                 </div>
-                <div class="footer-right">
 
-                </div>
             </footer>
         </div>
     </div>
