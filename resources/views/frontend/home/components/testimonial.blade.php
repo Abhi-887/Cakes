37bfa40632b37755770d59a38df2d05ca0edc8f4
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-09-13 11:52:39 +0530

    Update testimonial.blade.php

--- resources/views/frontend/home/components/testimonial.blade.php
+++ resources/views/frontend/home/components/testimonial.blade.php
@@ -92,7 +92,7 @@
                                 {{-- <img class="rounded-circle me-3" src="{{ asset($testimonial->image) }}"
                                     alt="Profile Picture"> --}}
                                 <h4 class="text-light fw-semibold my-3 fs-4">{{ $testimonial->name }}</h4>
-                                <p class="testimonial-content text-light fs-6 m-0">{{ $testimonial->title }}</p>
+                                <p class="testimonial-content text-light fs-6 m-0 pe-3">{{ $testimonial->title }}</p>
                             </div>
                         </div>
                     </div>
