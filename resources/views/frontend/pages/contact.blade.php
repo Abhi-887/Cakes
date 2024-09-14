40a5cb30627501d76fa03cfa2f25f3d75ea6b261
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-09-14 13:30:16 +0530

    a

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            BREADCRUMB START
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                BREADCRUMB START
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ==============================-->
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay py-5">
             <div class="container py-md-5 py-2">
@@ -18,13 +18,13 @@
         </div>
     </section>
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                BREADCRUMB END
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    BREADCRUMB END
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ==============================-->
 
 
     <!--=============================
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                CONTACT PAGE START
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ==============================-->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    CONTACT PAGE START
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            ==============================-->
 
 
     <style>
@@ -113,7 +113,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 mb-4 d-flex flex-column">
+            <div class="col-md-4 mb-4 mb-md-0 d-flex flex-column">
                 <div class="contact-card rounded-3 p-4 d-flex flex-column flex-grow-1">
                     <div class="contact-content flex-grow-1">
                         <div class="d-flex justify-content-between align-items-center">
@@ -183,7 +183,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 mb-4 mb-md- d-flex flex-column">
+            <div class="col-md-4 mb-4 mb-md-0 d-flex flex-column">
                 <div class="contact-card rounded-3 p-4 d-flex flex-column flex-grow-1">
                     <div class="contact-content flex-grow-1">
                         <div class="d-flex justify-content-between align-items-center">
@@ -201,7 +201,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 mb-4 d-flex flex-column">
+            <div class="col-md-4 mb-4 mb-md-0 d-flex flex-column">
                 <div class="contact-card rounded-3 p-4 d-flex flex-column flex-grow-1">
                     <div class="contact-content flex-grow-1">
                         <div class="d-flex justify-content-between align-items-center">
