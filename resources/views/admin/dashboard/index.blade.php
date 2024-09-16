7372ca69a9de9108d5fd0534918430332e6a0806
Abhishek Rajput <blazeneon1@gmail.com>
2024-09-16 12:41:14 +0530

    Update index.blade.php

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -316,7 +316,164 @@
 
 
         </div>
+        <div class="tf-section mb-30">
+            <!-- orders -->
+            <div class="wg-box">
+                <div class="flex items-center justify-between">
+                    <h5>Recent orders</h5>
+                    <div class="dropdown default">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="view-all">View all<i class="icon-chevron-down"></i></span>
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li>
+                                <a href="javascript:void(0);">3 days</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0);">7 days</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="wg-table table-product-overview t2">
+                    <ul class="table-title flex gap20 mb-14">
+                        <li>
+                            <div class="body-title">Product</div>
+                        </li>
+                        <li>
+                            <div class="body-title">Customer</div>
+                        </li>
+                        <li>
+                            <div class="body-title">Product ID</div>
+                        </li>
+                        <li>
+                            <div class="body-title">Quantity</div>
+                        </li>
+                        <li>
+                            <div class="body-title">Price</div>
+                        </li>
+                        <li>
+                            <div class="body-title">Status</div>
+                        </li>
+                    </ul>
+                    <div class="divider mb-14"></div>
+                    <ul class="flex flex-column gap10">
+                        <li class="product-item gap14">
+                            <div class="image no-bg">
+                                <img src="images/products/31.png" alt="">
+                            </div>
+                            <div class="flex items-center justify-between flex-grow gap20">
+                                <div class="name">
+                                    <a href="product-list.html" class="body-title-2">Taste of the Wild Formula Finder</a>
+                                </div>
+                                <div class="body-text">2,672</div>
+                                <div class="body-text">$28,672.36</div>
+                                <div class="body-text">X1</div>
+                                <div class="body-text">$28,672.36</div>
+                                <div>
+                                    <div class="block-available">Delivered</div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        <li class="product-item gap14">
+                            <div class="image no-bg">
+                                <img src="images/products/32.png" alt="">
+                            </div>
+                            <div class="flex items-center justify-between flex-grow gap20">
+                                <div class="name">
+                                    <a href="product-list.html" class="body-title-2">Proden Plaqueoff Dental Bites Dog, 150 G</a>
+                                </div>
+                                <div class="body-text">2,672</div>
+                                <div class="body-text">$28,672.36</div>
+                                <div class="body-text">X2</div>
+                                <div class="body-text">$28,672.36</div>
+                                <div>
+                                    <div class="block-available">Delivered</div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        <li class="product-item gap14">
+                            <div class="image no-bg">
+                                <img src="images/products/33.png" alt="">
+                            </div>
+                            <div class="flex items-center justify-between flex-grow gap20">
+                                <div class="name">
+                                    <a href="product-list.html" class="body-title-2">Zuke's Lil' Links Healthy Little Sausage Links for Dogs...</a>
+                                </div>
+                                <div class="body-text">2,672</div>
+                                <div class="body-text">$28,672.36</div>
+                                <div class="body-text">X1</div>
+                                <div class="body-text">$28,672.36</div>
+                                <div>
+                                    <div class="block-available">Delivered</div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        <li class="product-item gap14">
+                            <div class="image no-bg">
+                                <img src="images/products/34.png" alt="">
+                            </div>
+                            <div class="flex items-center justify-between flex-grow gap20">
+                                <div class="name">
+                                    <a href="product-list.html" class="body-title-2">Rachael Ray Nutrish Grain Free Chicken Drumstick...</a>
+                                </div>
+                                <div class="body-text">2,672</div>
+                                <div class="body-text">$28,672.36</div>
+                                <div class="body-text">X3</div>
+                                <div class="body-text">$28,672.36</div>
+                                <div>
+                                    <div class="block-available">Delivered</div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        <li class="product-item gap14">
+                            <div class="image no-bg">
+                                <img src="images/products/35.png" alt="">
+                            </div>
+                            <div class="flex items-center justify-between flex-grow gap20">
+                                <div class="name">
+                                    <a href="product-list.html" class="body-title-2">Fruitables Dog Treats Sweet Potato & Pecan Flavor</a>
+                                </div>
+                                <div class="body-text">2,672</div>
+                                <div class="body-text">$28,672.36</div>
+                                <div class="body-text">X2</div>
+                                <div class="body-text">$28,672.36</div>
+                                <div>
+                                    <div class="block-available">Delivered</div>
+                                </div>
+                            </div>
+                        </li>
 
+                    </ul>
+                </div>
+                <div class="divider"></div>
+                <div class="flex items-center justify-between flex-wrap gap10">
+                    <div class="text-tiny">Showing 5 entries</div>
+                    <ul class="wg-pagination">
+                        <li>
+                            <a href="#"><i class="icon-chevron-left"></i></a>
+                        </li>
+                        <li>
+                            <a href="#">1</a>
+                        </li>
+                        <li class="active">
+                            <a href="#">2</a>
+                        </li>
+                        <li>
+                            <a href="#">3</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="icon-chevron-right"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /orders -->
+        </div>
 
         <div class="tf-section-3">
             <!-- orders -->
@@ -647,6 +804,7 @@
             </div>
             <!-- /new-comment -->
         </div>
+
 
 
 
