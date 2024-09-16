bd33896c88d50dd233bb02340d528e707d3aeb2d
Abhishek Rajput <blazeneon1@gmail.com>
2024-09-16 15:29:26 +0530

    Update index.blade.php

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -133,93 +133,6 @@
 
             <!-- /chart-default -->
         </div>
-        <div class="tf-section-4 mb-30">
-            <!-- New Customers -->
-            <div class="wg-chart-default">
-                <div class="flex items-center justify-between">
-                    <div class="flex items-center gap14">
-                        <div class="image type-white">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="52" viewBox="0 0 48 52" fill="none">
-                                <path d="M19.1094 2.12943C22.2034 0.343099 26.0154 0.343099 29.1094 2.12943L42.4921 9.85592C45.5861 11.6423 47.4921 14.9435 47.4921 18.5162V33.9692C47.4921 37.5418 45.5861 40.8431 42.4921 42.6294L29.1094 50.3559C26.0154 52.1423 22.2034 52.1423 19.1094 50.3559L5.72669 42.6294C2.63268 40.8431 0.726688 37.5418 0.726688 33.9692V18.5162C0.726688 14.9435 2.63268 11.6423 5.72669 9.85592L19.1094 2.12943Z" fill="#22C55E" />
-                            </svg>
-                            <i class="icon-users"></i>
-                        </div>
-                        <div>
-                            <div class="body-text mb-2">New Customers (Last Week)</div>
-                            <h4>{{ number_format($newCustomers) }}</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Total Customers -->
-            <div class="wg-chart-default">
-                <div class="flex items-center justify-between">
-                    <div class="flex items-center gap14">
-                        <div class="image type-white">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="52" viewBox="0 0 48 52" fill="none">
-                                <path d="M19.1094 2.12943C22.2034 0.343099 26.0154 0.343099 29.1094 2.12943L42.4921 9.85592C45.5861 11.6423 47.4921 14.9435 47.4921 18.5162V33.9692C47.4921 37.5418 45.5861 40.8431 42.4921 42.6294L29.1094 50.3559C26.0154 52.1423 22.2034 52.1423 19.1094 50.3559L5.72669 42.6294C2.63268 40.8431 0.726688 37.5418 0.726688 33.9692V18.5162C0.726688 14.9435 2.63268 11.6423 5.72669 9.85592L19.1094 2.12943Z" fill="#FF5200" />
-                            </svg>
-                            <i class="icon-users"></i>
-                        </div>
-                        <div>
-                            <div class="body-text mb-2">Total Customers</div>
-                            <h4>{{ number_format($totalCustomers) }}</h4>
-                        </div>
-                    </div><div class="tf-section-4 mb-30">
-            <!-- New Customers -->
-            <div class="wg-chart-default">
-                <div class="flex items-center justify-between">
-                    <div class="flex items-center gap14">
-                        <div class="image type-white">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="52" viewBox="0 0 48 52" fill="none">
-                                <path d="M19.1094 2.12943C22.2034 0.343099 26.0154 0.343099 29.1094 2.12943L42.4921 9.85592C45.5861 11.6423 47.4921 14.9435 47.4921 18.5162V33.9692C47.4921 37.5418 45.5861 40.8431 42.4921 42.6294L29.1094 50.3559C26.0154 52.1423 22.2034 52.1423 19.1094 50.3559L5.72669 42.6294C2.63268 40.8431 0.726688 37.5418 0.726688 33.9692V18.5162C0.726688 14.9435 2.63268 11.6423 5.72669 9.85592L19.1094 2.12943Z" fill="#22C55E" />
-                            </svg>
-                            <i class="icon-users"></i>
-                        </div>
-                        <div>
-                            <div class="body-text mb-2">New Customers (Last Week)</div>
-                            <h4>{{ number_format($newCustomers) }}</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Conversion Rate -->
-            <div class="wg-chart-default">
-                <div class="flex items-center justify-between">
-                    <div class="flex items-center gap14">
-                        <div class="image type-white">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="52" viewBox="0 0 48 52" fill="none">
-                                <path d="M19.1094 2.12943C22.2034 0.343099 26.0154 0.343099 29.1094 2.12943L42.4921 9.85592C45.5861 11.6423 47.4921 14.9435 47.4921 18.5162V33.9692C47.4921 37.5418 45.5861 40.8431 42.4921 42.6294L29.1094 50.3559C26.0154 52.1423 22.2034 52.1423 19.1094 50.3559L5.72669 42.6294C2.63268 40.8431 0.726688 37.5418 0.726688 33.9692V18.5162C0.726688 14.9435 2.63268 11.6423 5.72669 9.85592L19.1094 2.12943Z" fill="#CBD5E1" />
-                            </svg>
-                            <i class="icon-stats"></i>
-                        </div>
-                        <div>
-                            <div class="body-text mb-2">Conversion Rate</div>
-                            <h4>{{ number_format($conversionRate, 2) }}%</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Bounce Rate -->
-            <div class="wg-chart-default">
-                <div class="flex items-center justify-between">
-                    <div class="flex items-center gap14">
-                        <div class="image type-white">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="52" viewBox="0 0 48 52" fill="none">
-                                <path d="M19.1094 2.12943C22.2034 0.343099 26.0154 0.343099 29.1094 2.12943L42.4921 9.85592C45.5861 11.6423 47.4921 14.9435 47.4921 18.5162V33.9692C47.4921 37.5418 45.5861 40.8431 42.4921 42.6294L29.1094 50.3559C26.0154 52.1423 22.2034 52.1423 19.1094 50.3559L5.72669 42.6294C2.63268 40.8431 0.726688 37.5418 0.726688 33.9692V18.5162C0.726688 14.9435 2.63268 11.6423 5.72669 9.85592L19.1094 2.12943Z" fill="#FF5200" />
-                            </svg>
-                            <i class="icon-stats"></i>
-                        </div>
-                        <div>
-                            <div class="body-text mb-2">Bounce Rate</div>
-                            <h4>{{ number_format($bounceRate, 2) }}%</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
         <div class="tf-section-5 mb-30">
             <!-- chart -->
             {{-- <div class="wg-box">
@@ -917,7 +830,93 @@
             </div>
 
 
+            <div class="tf-section-4 mb-30">
+                <!-- New Customers -->
+                <div class="wg-chart-default">
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center gap14">
+                            <div class="image type-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="52" viewBox="0 0 48 52" fill="none">
+                                    <path d="M19.1094 2.12943C22.2034 0.343099 26.0154 0.343099 29.1094 2.12943L42.4921 9.85592C45.5861 11.6423 47.4921 14.9435 47.4921 18.5162V33.9692C47.4921 37.5418 45.5861 40.8431 42.4921 42.6294L29.1094 50.3559C26.0154 52.1423 22.2034 52.1423 19.1094 50.3559L5.72669 42.6294C2.63268 40.8431 0.726688 37.5418 0.726688 33.9692V18.5162C0.726688 14.9435 2.63268 11.6423 5.72669 9.85592L19.1094 2.12943Z" fill="#22C55E" />
+                                </svg>
+                                <i class="icon-users"></i>
+                            </div>
+                            <div>
+                                <div class="body-text mb-2">New Customers (Last Week)</div>
+                                <h4>{{ number_format($newCustomers) }}</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+                <!-- Total Customers -->
+                <div class="wg-chart-default">
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center gap14">
+                            <div class="image type-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="52" viewBox="0 0 48 52" fill="none">
+                                    <path d="M19.1094 2.12943C22.2034 0.343099 26.0154 0.343099 29.1094 2.12943L42.4921 9.85592C45.5861 11.6423 47.4921 14.9435 47.4921 18.5162V33.9692C47.4921 37.5418 45.5861 40.8431 42.4921 42.6294L29.1094 50.3559C26.0154 52.1423 22.2034 52.1423 19.1094 50.3559L5.72669 42.6294C2.63268 40.8431 0.726688 37.5418 0.726688 33.9692V18.5162C0.726688 14.9435 2.63268 11.6423 5.72669 9.85592L19.1094 2.12943Z" fill="#FF5200" />
+                                </svg>
+                                <i class="icon-users"></i>
+                            </div>
+                            <div>
+                                <div class="body-text mb-2">Total Customers</div>
+                                <h4>{{ number_format($totalCustomers) }}</h4>
+                            </div>
+                        </div><div class="tf-section-4 mb-30">
+                <!-- New Customers -->
+                <div class="wg-chart-default">
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center gap14">
+                            <div class="image type-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="52" viewBox="0 0 48 52" fill="none">
+                                    <path d="M19.1094 2.12943C22.2034 0.343099 26.0154 0.343099 29.1094 2.12943L42.4921 9.85592C45.5861 11.6423 47.4921 14.9435 47.4921 18.5162V33.9692C47.4921 37.5418 45.5861 40.8431 42.4921 42.6294L29.1094 50.3559C26.0154 52.1423 22.2034 52.1423 19.1094 50.3559L5.72669 42.6294C2.63268 40.8431 0.726688 37.5418 0.726688 33.9692V18.5162C0.726688 14.9435 2.63268 11.6423 5.72669 9.85592L19.1094 2.12943Z" fill="#22C55E" />
+                                </svg>
+                                <i class="icon-users"></i>
+                            </div>
+                            <div>
+                                <div class="body-text mb-2">New Customers (Last Week)</div>
+                                <h4>{{ number_format($newCustomers) }}</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Conversion Rate -->
+                <div class="wg-chart-default">
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center gap14">
+                            <div class="image type-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="52" viewBox="0 0 48 52" fill="none">
+                                    <path d="M19.1094 2.12943C22.2034 0.343099 26.0154 0.343099 29.1094 2.12943L42.4921 9.85592C45.5861 11.6423 47.4921 14.9435 47.4921 18.5162V33.9692C47.4921 37.5418 45.5861 40.8431 42.4921 42.6294L29.1094 50.3559C26.0154 52.1423 22.2034 52.1423 19.1094 50.3559L5.72669 42.6294C2.63268 40.8431 0.726688 37.5418 0.726688 33.9692V18.5162C0.726688 14.9435 2.63268 11.6423 5.72669 9.85592L19.1094 2.12943Z" fill="#CBD5E1" />
+                                </svg>
+                                <i class="icon-stats"></i>
+                            </div>
+                            <div>
+                                <div class="body-text mb-2">Conversion Rate</div>
+                                <h4>{{ number_format($conversionRate, 2) }}%</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Bounce Rate -->
+                <div class="wg-chart-default">
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-center gap14">
+                            <div class="image type-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="52" viewBox="0 0 48 52" fill="none">
+                                    <path d="M19.1094 2.12943C22.2034 0.343099 26.0154 0.343099 29.1094 2.12943L42.4921 9.85592C45.5861 11.6423 47.4921 14.9435 47.4921 18.5162V33.9692C47.4921 37.5418 45.5861 40.8431 42.4921 42.6294L29.1094 50.3559C26.0154 52.1423 22.2034 52.1423 19.1094 50.3559L5.72669 42.6294C2.63268 40.8431 0.726688 37.5418 0.726688 33.9692V18.5162C0.726688 14.9435 2.63268 11.6423 5.72669 9.85592L19.1094 2.12943Z" fill="#FF5200" />
+                                </svg>
+                                <i class="icon-stats"></i>
+                            </div>
+                            <div>
+                                <div class="body-text mb-2">Bounce Rate</div>
+                                <h4>{{ number_format($bounceRate, 2) }}%</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             <!-- Average Session Duration -->
             <div>
