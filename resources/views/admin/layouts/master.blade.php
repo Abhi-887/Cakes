beb803b82ebc62b929127f863dbd7ac70db739af
Abhishek Rajput <blazeneon1@gmail.com>
2024-09-17 15:04:29 +0530

    Update master.blade.php

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -45,10 +45,10 @@
     <link rel="shortcut icon" href="{{ asset('admin2/assets/images/favicon.png') }}">
     <link rel="apple-touch-icon-precomposed" href="{{ asset('admin2/assets/images/favicon.png') }}">
     <link rel="stylesheet" href="{{ asset('admin/assets/modules/summernote/summernote-bs4.css') }}">
-{{--
+
     <!-- Template CSS -->
     <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('admin/assets/css/components.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/components.css') }}">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
     <!-- Start GA -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
