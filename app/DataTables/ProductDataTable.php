36795cfcaea3c7f4360ec759f0f293b8f7e332b0
Abhishek Rajput <blazeneon1@gmail.com>
2024-09-17 14:57:59 +0530

    Update ProductDataTable.php

--- app/DataTables/ProductDataTable.php
+++ app/DataTables/ProductDataTable.php
@@ -3,7 +3,6 @@
 namespace App\DataTables;
 
 use App\Models\Product;
-
 use Illuminate\Database\Eloquent\Builder as QueryBuilder;
 use Yajra\DataTables\EloquentDataTable;
 use Yajra\DataTables\Html\Builder as HtmlBuilder;
@@ -18,28 +17,21 @@ class ProductDataTable extends DataTable
     /**
      * Build the DataTable class.
      *
-     * @param QueryBuilder $query Results from query() method.<i class="fa-solid fa-link"></i>
-
-
+     * @param QueryBuilder $query Results from query() method.
      */
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
         return (new EloquentDataTable($query))
             ->addColumn('action', function ($query) {
+                $edit = "<a href='" . route('admin.product.edit', $query->id) . "' class='btn btn-outline-primary btn-sm'><i class='fas fa-edit'></i> Edit</a>";
+                $delete = "<a href='" . route('admin.product.destroy', $query->id) . "' class='btn btn-outline-danger btn-sm mx-1 delete-item'><i class='fas fa-trash'></i> Delete</a>";
 
-
-                $edit = "<a href='" . route('admin.product.edit', $query->id) . "' class='btn btn-primary'><i class='fas fa-edit'></i></a>";
-                $delete = "<a href='" . route('admin.product.destroy', $query->id) . "' class='mx-2 btn btn-danger delete-item'><i class='fas fa-trash'></i></a>";
-
-                $more = '<div class="btn-group dropleft">
-                <button type="button" class="btn btn-dark dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-cog"></i></button>
-                <div class="dropdown-menu dropleft" x-placement="left-start" style="position: absolute; transform: translate3d(-2px, 0px, 0px); top: 0px; left: 0px; will-change: transform;">
-                  <a class="dropdown-item" href="' . route('admin.product-gallery.show-index', $query->id) . '"><i class="far fa-regular fa-image"></i> Product Gallery</a>
-
-                   <a class="dropdown-item has-icon" href="' . route('admin.products-variant.index', ['product' => $query->id]) . '"><i class="far fa-solid fa-tag"></i> Variants</a>
-
-
-                </div>
+                $more = '<div class="btn-group dropstart">
+                <button type="button" class="btn btn-outline-dark btn-sm dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-cog"></i> More</button>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="' . route('admin.product-gallery.show-index', $query->id) . '"><i class="fas fa-image"></i> Product Gallery</a></li>
+                  <li><a class="dropdown-item" href="' . route('admin.products-variant.index', ['product' => $query->id]) . '"><i class="fas fa-tags"></i> Variants</a></li>
+                </ul>
               </div>';
 
                 return $edit . $delete . $more;
@@ -52,20 +44,20 @@ class ProductDataTable extends DataTable
             })
             ->addColumn('status', function ($query) {
                 if ($query->status === 1) {
-                    return '<span class="badge badge-primary">Yes</span>';
+                    return '<span class="badge bg-success">Active</span>';
                 } else {
-                    return '<span class="badge badge-danger">No</span>';
+                    return '<span class="badge bg-danger">Inactive</span>';
                 }
             })
             ->addColumn('show_at_home', function ($query) {
                 if ($query->show_at_home === 1) {
-                    return '<span class="badge badge-primary">Yes</span>';
+                    return '<span class="badge bg-info">Yes</span>';
                 } else {
-                    return '<span class="badge badge-danger">No</span>';
+                    return '<span class="badge bg-secondary">No</span>';
                 }
             })
             ->addColumn('image', function ($query) {
-                return '<img width="60px" src="' . asset($query->thumb_image) . '">';
+                return '<img class="img-thumbnail" width="60px" src="' . asset($query->thumb_image) . '">';
             })
             ->addColumn('category', function ($query) {
                 return $query->category->name;
@@ -108,36 +100,25 @@ class ProductDataTable extends DataTable
      * Get the dataTable columns definition.
      */
     public function getColumns(): array
-{
-    return [
-
-        Column::make('id')
-            ->title('ID'),
-        Column::make('image')
-            ->title('Image'),
-        Column::make('name')
-            ->title('Product Name'),
-        Column::make('category')
-            ->title('Category'),
-        Column::make('price')
-            ->title('Price'),
-        Column::make('offer_price')
-            ->title('Offer Price'),
-        Column::make('quantity')
-            ->title('Available Qty'),
-        Column::make('show_at_home')
-            ->title('Show at Home'),
-        Column::make('status')
-            ->title('List Product'),
-        Column::computed('action')
-            ->title('Actions')
-            ->exportable(false)
-            ->printable(false)
-            ->width(220)
-            ->addClass('text-center'),
-    ];
-}
-
+    {
+        return [
+            Column::make('id')->title('ID'),
+            Column::make('image')->title('Image'),
+            Column::make('name')->title('Product Name'),
+            Column::make('category')->title('Category'),
+            Column::make('price')->title('Price'),
+            Column::make('offer_price')->title('Offer Price'),
+            Column::make('quantity')->title('Available Qty'),
+            Column::make('show_at_home')->title('Show at Home'),
+            Column::make('status')->title('List Product'),
+            Column::computed('action')
+                ->title('Actions')
+                ->exportable(false)
+                ->printable(false)
+                ->width(220)
+                ->addClass('text-center'),
+        ];
+    }
 
     /**
      * Get the filename for export.
