ad8fad3af309f74042c5f8a105dd493c7bb6f3ae
Abhishek Rajput <blazeneon1@gmail.com>
2024-09-17 14:50:09 +0530

    Update index.blade.php

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -67,43 +67,20 @@
     </div>
 
     <div class="wg-box">
-        <div class="title-box">
-            <i class="icon-coffee"></i>
-            <div class="body-text">
-                Tip: Search by Product ID: Each product is provided with a unique ID, which you can rely on to find the exact product you need.
-            </div>
-        </div>
+
 
         <div class="flex items-center justify-between gap10 flex-wrap">
-            <div class="wg-filter flex-grow">
-                <div class="show">
-                    <div class="text-tiny">Showing</div>
-                    <div class="select">
-                        <select id="product-length-change" class="form-control">
-                            <option value="10">10</option>
-                            <option value="20">20</option>
-                            <option value="30">30</option>
-                        </select>
-                    </div>
-                    <div class="text-tiny">entries</div>
-                </div>
-                <form class="form-search">
-                    <fieldset class="name">
-                        <input type="text" placeholder="Search here..." id="product-search" class="form-control" name="name">
-                    </fieldset>
-                    <div class="button-submit">
-                        <button type="button" id="search-btn" class="btn btn-primary"><i class="icon-search"></i></button>
-                    </div>
-                </form>
-            </div>
+
             <a class="tf-button style-1 w208" href="{{ route('admin.product.create') }}">
                 <i class="icon-plus"></i> Add new
             </a>
+
         </div>
 
         <!-- DataTable -->
         <div class="wg-table table-product-list">
-            {!! $dataTable->table(['id' => 'products-table', 'class' => 'table table-bordered table-hover table-striped']) !!}
+
+            {!! $dataTable->table(['id' => 'products-table']) !!}
         </div>
     </div>
 </div>
