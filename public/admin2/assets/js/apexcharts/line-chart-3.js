40aad3cb92e7bbaf1385a89de1e8f120a7a7d7e5
Abhishek Rajput <blazeneon1@gmail.com>
2024-09-18 17:52:19 +0530

    Update line-chart-3.js

--- public/admin2/assets/js/apexcharts/line-chart-3.js
+++ public/admin2/assets/js/apexcharts/line-chart-3.js
@@ -1,16 +1,16 @@
 (function ($) {
-  
+
     var tfLineChart = (function () {
-  
+
       var chartBar = function () {
-      
+
         var options = {
             series: [
               {
                 data: [20, 50, 7, 100, 30, 80, 100],
               },
             ],
-            colors: ["#CBD5E1"],
+            colors: ["#4cd0e1"],
             chart: {
               type: "area",
               maxWidth: 96,
@@ -19,7 +19,7 @@
             },
             plotOptions: { bar: { columnWidth: "50%" } },
             xaxis: { crosshairs: { width: 1 } },
-  
+
             stroke: {
               show: true,
               curve: "smooth",
@@ -28,7 +28,7 @@
               width: 3,
               dashArray: 0,
             },
-  
+
             tooltip: {
               fixed: { enabled: !1 },
               x: { show: !1 },
@@ -59,23 +59,23 @@
           chart.render();
         }
       };
-  
+
       /* Function ============ */
       return {
         init: function () {},
-  
+
         load: function () {
           chartBar();
         },
         resize: function () {},
       };
     })();
-  
+
     jQuery(document).ready(function () {});
-  
+
     jQuery(window).on("load", function () {
       tfLineChart.load();
     });
-  
+
     jQuery(window).on("resize", function () {});
 })(jQuery);
