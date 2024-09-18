4bf24ce3e2ce4ea64d044829cecd71fd99f98b26
abhisheksinghbisht <abhishekbisht@drupalbuilder.com>
2024-09-18 11:19:12 +0530

    Update about.blade.php

--- resources/views/frontend/pages/about.blade.php
+++ resources/views/frontend/pages/about.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <!--=============================
-                            BREADCRUMB START
-                        ==============================-->
+                                    BREADCRUMB START
+                                ==============================-->
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay py-5">
             <div class="container py-md-5 py-2">
@@ -18,13 +18,13 @@
         </div>
     </section>
     <!--=============================
-                            BREADCRUMB END
-                        ==============================-->
+                                    BREADCRUMB END
+                                ==============================-->
 
 
     <!--=============================
-                            ABOUT PAGE START
-                        ============================== -->
+                                    ABOUT PAGE START
+                                ============================== -->
     <section class="fp__about_us mt_120 xs_mt_90">
         <div class="container custom-container ">
             <div class="row">
@@ -39,6 +39,26 @@
                     </div>
                 </div>
                 <div class="col-xl-6 col-lg-5 wow fadeInUp" data-wow-duration="1s">
+                    <section class="fp__about_video py-5">
+                        <div class="container wow fadeInUp" data-wow-duration="1s">
+                            <div class="fp__about_video_bg"
+                                style="background: url({{ getYtThumbnail(@$about->video_link, 'high') }});">
+                                <div class="fp__about_video_overlay">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="fp__about_video_text">
+                                                <p>Watch Videos</p>
+                                                <a class="play_btn venobox" data-autoplay="true" data-vbtype="video"
+                                                    href="{{ @$about->video_link }}">
+                                                    <i class=" fas fa-play"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
                     <div class="fp__about_">
                         <img src="{{ asset(@$about->image) }}" alt="about us" class="img-fluid w-100">
                     </div>
@@ -66,14 +86,14 @@
     </section>
 
     <!--=============================
-                        WHY CHOOSE START
-                    ==============================-->
+            WHY CHOOSE START
+        ==============================-->
     <!-- @include('frontend.home.components.why-choose') -->
     <!--=============================
-                        WHY CHOOSE END
-                    ==============================-->
+            WHY CHOOSE END
+        ==============================-->
 
-    <section class="fp__about_video mt_100 xs_mt_70 my-5">
+    {{-- <section class="fp__about_video py-5">
         <div class="container wow fadeInUp" data-wow-duration="1s">
             <div class="fp__about_video_bg" style="background: url({{ getYtThumbnail(@$about->video_link, 'high') }});">
                 <div class="fp__about_video_overlay">
@@ -91,31 +111,31 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> --}}
 
     <!--=============================
-                        TEAM START
-                        ==============================-->
+                                TEAM START
+                                ==============================-->
     <!--  @include('frontend.home.components.team')-->
     <!--=============================
-                        TEAM END
-                        ==============================-->
+                                TEAM END
+                                ==============================-->
 
     <!--=============================
-                            COUNTER START
-                        ==============================-->
+                                    COUNTER START
+                                ==============================-->
     <!--  @include('frontend.home.components.counter')-->
     <!--=============================
-                                COUNTER END
-                        ==============================-->
+                                        COUNTER END
+                                ==============================-->
     <!--=============================
-                           TESTIMONIAL  START
-                        ==============================-->
+                                   TESTIMONIAL  START
+                                ==============================-->
     <!--  @include('frontend.home.components.testimonial')-->
     <!--=============================
-                                TESTIMONIAL END
-                        ==============================-->
+                                        TESTIMONIAL END
+                                ==============================-->
     <!--=============================
-                            ABOUT PAGE END
-                        ==============================-->
+                                    ABOUT PAGE END
+                                ==============================-->
 @endsection
