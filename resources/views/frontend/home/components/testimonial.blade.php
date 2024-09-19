1d26fb6597dd7562f1d1dca253c3ca65f7630b73
Abhishek Rajput <blazeneon1@gmail.com>
2024-09-19 12:32:15 +0530

    Update testimonial.blade.php

--- resources/views/frontend/home/components/testimonial.blade.php
+++ resources/views/frontend/home/components/testimonial.blade.php
@@ -103,8 +103,6 @@
     </div>
 </div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
 <script>
     $(document).ready(function() {
         $('.testimonial-slider .row').slick({
