0a5e6b3b35b628c386022f7ba4fbf8728e5b846b
drupalbuilder-shubham <royshubham0921@gmail.com>
2024-10-04 17:27:34 +0530

    Update blog-details.blade.php

--- resources/views/frontend/pages/blog-details.blade.php
+++ resources/views/frontend/pages/blog-details.blade.php
@@ -16,14 +16,14 @@
 @endphp
 </pre> --}}
     <!--=============================
-  BREADCRUMB START
- ==============================-->
+          BREADCRUMB START
+         ==============================-->
     <section class="fp__breadcrumb" style="background: url({{ asset(config('settings.breadcrumb')) }});">
         <div class="fp__breadcrumb_overlay py-5">
             <div class="container py-md-5 py-2">
                 <div class="fp__breadcrumb_text">
                     <!--<h1>blog details</h1>-->
-					<h2>3D Cakes - Latest News</h2>
+                    <h2>3D Cakes - Latest News</h2>
                     <ul>
                         <li><a href="index.html">home</a></li>
                         <li><a href="#">blog details</a></li>
@@ -33,12 +33,12 @@
         </div>
     </section>
     <!--=============================                                                                                                                        BREADCRUMB END
-	==============================-->
+         ==============================-->
 
 
     <!--=========================
-	BLOG DETAILS START
-	==========================-->
+         BLOG DETAILS START
+         ==========================-->
     <section class="fp__blog_details mt_120 xs_mt_90 mb_100 xs_mb_70">
         <div class="container">
             <div class="row">
@@ -57,10 +57,12 @@
                                     {{ date('d m Y', strtotime($blog->created_at)) }}
                                 </li>
                             </ul>
-                           <!-- <h2>{!! $blog->title !!}</h2>-->
-							<h1>{!! $blog->title !!}</h1>
+                            <!-- <h2>{!! $blog->title !!}</h2>-->
+                            <h1>{!! $blog->title !!}</h1>
 
-                           <div><p class="fw-normal color-light-gray"> {!! $blog->description !!}</p></div>
+                            <div>
+                                <p class="fw-normal color-light-gray"> {!! $blog->description !!}</p>
+                            </div>
 
                             <div class="blog_tags_share d-flex flex-wrap justify-content-between align-items-center">
                                 <div class="tags d-flex flex-wrap align-items-center">
@@ -83,29 +85,29 @@
                         </div>
                     </div>
 
-                 <!--   <ul class="blog_det_button mt_100 xs_mt_70 wow fadeInUp" data-wow-duration="1s">
-                        @if ($nextBlog)
-                            <li>
-                                <a href="{{ route('blogs.details', $nextBlog->slug) }}">
-                                    <img src="{{ asset($nextBlog->image) }}" alt="button img" class="img-fluid w-100">
-                                    <p>{{ truncate($nextBlog->title) }}
-                                        <span> <i class="far fa-long-arrow-left"></i> Previous</span>
-                                    </p>
-                                </a>
-                            </li>
-                        @endif
-                        @if ($previousBlog)
-                            <li>
-                                <a href="{{ route('blogs.details', $previousBlog->slug) }}">
-                                    <p>{{ truncate($previousBlog->title) }}
-                                        <span>next <i class="far fa-long-arrow-right"></i></span>
-                                    </p>
-                                    <img src="{{ asset($previousBlog->image) }}" alt="button img" class="img-fluid w-100">
-                                </a>
-                            </li>
-                        @endif
-                    </ul>-->
-{{-- <ul class="blog_det_button mt_100 xs_mt_70 wow fadeInUp d-flex justify-content-between" data-wow-duration="1s">
+                    <!--   <ul class="blog_det_button mt_100 xs_mt_70 wow fadeInUp" data-wow-duration="1s">
+                                @if ($nextBlog)
+    <li>
+                                        <a href="{{ route('blogs.details', $nextBlog->slug) }}">
+                                            <img src="{{ asset($nextBlog->image) }}" alt="button img" class="img-fluid w-100">
+                                            <p>{{ truncate($nextBlog->title) }}
+                                                <span> <i class="far fa-long-arrow-left"></i> Previous</span>
+                                            </p>
+                                        </a>
+                                    </li>
+    @endif
+                                @if ($previousBlog)
+    <li>
+                                        <a href="{{ route('blogs.details', $previousBlog->slug) }}">
+                                            <p>{{ truncate($previousBlog->title) }}
+                                                <span>next <i class="far fa-long-arrow-right"></i></span>
+                                            </p>
+                                            <img src="{{ asset($previousBlog->image) }}" alt="button img" class="img-fluid w-100">
+                                        </a>
+                                    </li>
+    @endif
+                            </ul>-->
+                    {{-- <ul class="blog_det_button mt_100 xs_mt_70 wow fadeInUp d-flex justify-content-between" data-wow-duration="1s">
     @if ($nextBlog)
         <li class="blog-nav-item text-end">
             <a href="{{ route('blogs.details', $nextBlog->slug) }}" class="blog-link">
@@ -134,100 +136,106 @@
     @endif
 </ul> --}}
 
-<div class="container mt-5 wow fadeInUp" data-wow-duration="1s">
-  <div class="row align-items-stretch">
-    @if ($nextBlog)
-      <div class="col-md-6">
-        <div class="card h-100">
-			<div class="row">
-			  <div class="col-4"><img class="h-auto img-fluid w-100 img-blog" src="{{ asset($nextBlog->image) }}"> </div>
-			  <div class="col-8">
-					<div class="card-body d-flex flex-column">
-						<p class="card-title">{{ truncate($nextBlog->title) }}</p>
-						<div class="mt-auto d-flex justify-content-between align-items-center">
-						  <a href="#" class="btn btn-outline-secondary">← Previous</a>
-						</div>
-				  </div>
-			  </div>
-			</div>
-		</div>
-      </div>
-    @endif
-    @if ($previousBlog)
-      <div class="col-md-6">
-        <div class="card h-100 d-flex">
-          <div class="row">
-
-          <div class="col-8">  <div class="card-body d-flex flex-column">
-            <p class="card-title">{{ truncate($previousBlog->title) }}</p>
-            <div class="mt-auto d-flex justify-content-between align-items-center">
-              <a href="{{ route('blogs.details', $previousBlog->slug) }}" class="btn btn-outline-secondary">Next →</a>
-            </div>
-          </div>
-		   </div>
-		   <div class="col-4"><img class="h-auto img-fluid w-100 img-blog" src="{{ asset($previousBlog->image) }}"></div>
-        </div>
-      </div>
-  </div>
-   @endif
-</div>
-
-
-<style>
-.img-blog {
-  width: 110px !important;
-  height: 150px !important;
-  border-radius: 5px;
-}
-</style>
-
-
-
-
-                    <div class="fp__comment mt_100 xs_mt_70 wow fadeInUp" data-wow-duration="1s">
-                        <h4>{{ count($comments) }} Comments</h4>
-                        @foreach ($comments as $comment)
-                            <div class="fp__single_comment m-0 border-0">
-                                <img src="{{ asset($comment->user->avatar) }}" alt="review" class="img-fluid">
-                                <div class="fp__single_comm_text">
-                                    <h3>{{ $comment->user->name }}
-                                        <span>{{ date('d M Y', strtotime($comment->created_at)) }} </span>
-                                    </h3>
-                                    <p>{{ $comment->comment }}</p>
+                    <div class="container mt-5 wow fadeInUp" data-wow-duration="1s">
+                        <div class="row align-items-stretch">
+                            @if ($nextBlog)
+                                <div class="col-md-6">
+                                    <div class="card h-100">
+                                        <div class="row">
+                                            <div class="col-4"><img class="h-auto img-fluid w-100 img-blog"
+                                                    src="{{ asset($nextBlog->image) }}"> </div>
+                                            <div class="col-8">
+                                                <div class="card-body d-flex flex-column">
+                                                    <p class="card-title">{{ truncate($nextBlog->title) }}</p>
+                                                    <div class="mt-auto d-flex justify-content-between align-items-center">
+                                                        <a href="#" class="btn btn-outline-secondary">← Previous</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        @endforeach
+                            @endif
+                            @if ($previousBlog)
+                                <div class="col-md-6">
+                                    <div class="card h-100 d-flex">
+                                        <div class="row">
 
-                        @if ($comments->hasPages())
-                            <div class="fp__pagination mt_60">
+                                            <div class="col-8">
+                                                <div class="card-body d-flex flex-column">
+                                                    <p class="card-title">{{ truncate($previousBlog->title) }}</p>
+                                                    <div class="mt-auto d-flex justify-content-between align-items-center">
+                                                        <a href="{{ route('blogs.details', $previousBlog->slug) }}"
+                                                            class="btn btn-outline-secondary">Next →</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-4"><img class="h-auto img-fluid w-100 img-blog"
+                                                    src="{{ asset($previousBlog->image) }}"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endif
+                        </div>
+
+
+                        <style>
+                            .img-blog {
+                                width: 110px !important;
+                                height: 150px !important;
+                                border-radius: 5px;
+                            }
+                        </style>
+
+
+
+
+                        <div class="fp__comment mt_100 xs_mt_70 wow fadeInUp" data-wow-duration="1s">
+                            <h4>{{ count($comments) }} Comments</h4>
+                            @foreach ($comments as $comment)
+                                <div class="fp__single_comment m-0 border-0">
+                                    <img src="{{ asset($comment->user->avatar) }}" alt="review" class="img-fluid">
+                                    <div class="fp__single_comm_text">
+                                        <h3>{{ $comment->user->name }}
+                                            <span>{{ date('d M Y', strtotime($comment->created_at)) }} </span>
+                                        </h3>
+                                        <p>{{ $comment->comment }}</p>
+                                    </div>
+                                </div>
+                            @endforeach
+
+                            @if ($comments->hasPages())
+                                <div class="fp__pagination mt_60">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            {{ $comments->links() }}
+                                        </div>
+                                    </div>
+                                </div>
+                            @endif
+
+                        </div>
+
+                        <div class="comment_input mt_100 xs_mt_70 wow fadeInUp" data-wow-duration="1s">
+                            <h4>Leave A Comment</h4>
+                            <p>Required fields are marked *</p>
+                            <form action="{{ route('blogs.comment.store', $blog->id) }}" method="POST">
+                                @csrf
                                 <div class="row">
-                                    <div class="col-12">
-                                        {{ $comments->links() }}
+
+                                    <div class="col-xl-12">
+                                        <label>comment *</label>
+                                        <div class="fp__contact_form_input textarea">
+                                            <span><i class="fal fa-user-alt"></i></span>
+                                            <textarea rows="5" placeholder="Your Comment" name="comment"></textarea>
+                                        </div>
+                                        <button type="submit"
+                                            class="common_btn mt_20 text-white background-light-gray">Submit
+                                            comment</button>
                                     </div>
                                 </div>
-                            </div>
-                        @endif
-
-                    </div>
-
-                    <div class="comment_input mt_100 xs_mt_70 wow fadeInUp" data-wow-duration="1s">
-                        <h4>Leave A Comment</h4>
-                        <p>Required fields are marked *</p>
-                        <form action="{{ route('blogs.comment.store', $blog->id) }}" method="POST">
-                            @csrf
-                            <div class="row">
-
-                                <div class="col-xl-12">
-                                    <label>comment *</label>
-                                    <div class="fp__contact_form_input textarea">
-                                        <span><i class="fal fa-user-alt"></i></span>
-                                        <textarea rows="5" placeholder="Your Comment" name="comment"></textarea>
-                                    </div>
-                                    <button type="submit" class="common_btn mt_20 text-white background-light-gray">Submit
-                                        comment</button>
-                                </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4">
@@ -279,6 +287,6 @@
     </section>
 
     <!--=========================
-	BLOG DETAILS END
-	==========================-->
+         BLOG DETAILS END
+         ==========================-->
 @endsection
