483cad686f60d569ee7180b6169c032526a145e0
drupalbuilder-shubham <royshubham0921@gmail.com>
2024-10-04 17:03:13 +0530

    zdcf

--- resources/views/frontend/home/components/blog.blade.php
+++ resources/views/frontend/home/components/blog.blade.php
@@ -36,7 +36,7 @@
                                 <p class="fw-normal color-light-gray mt-3">{!! truncate($blog->description, 100) !!}</p>
                                 <a href="{{ route('blogs.details', $blog->slug) }}"
                                     class="blog-btn fw-light rounded-3 color-dark-gray fs-14">
-                                    Read More <i class="fas fa-arrow-right ms-1 fw-semibold"></i>
+                                    Read More <i class="fas fa-arrow-right ms-1 fw-semibold unic"></i>
                                 </a>
                             </div>
                         </div>
