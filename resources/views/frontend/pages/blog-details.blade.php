572bd0c609e691026f932e95d9bc861433b894d3
drupalbuilder-shubham <royshubham0921@gmail.com>
2024-10-05 11:02:45 +0530

    Update blog-details.blade.php

--- resources/views/frontend/pages/blog-details.blade.php
+++ resources/views/frontend/pages/blog-details.blade.php
@@ -165,7 +165,7 @@
                                         <div class="col-8 d-flex">
                                             <div class="card h-100 w-100 d-flex flex-column border-0">
                                                 <div class="card-body d-flex flex-column justify-content-between">
-                                                    <p class="card-title mb-4">{{ truncate($nextBlog->title) }}</p>
+                                                    <p class="card-title mb-4 text-end">{{ truncate($nextBlog->title) }}</p>
                                                     <div class="mt-auto d-flex justify-content-end align-items-center">
                                                         <a href="#" class="btn btn-outline-secondary">← Previous</a>
                                                     </div>
