40f09e8b620d40f1682216274690dd3fc6de4b0c
drupalbuilder-shubham <royshubham0921@gmail.com>
2024-10-08 16:27:59 +0530

    Update menu.blade.php

--- resources/views/frontend/layouts/menu.blade.php
+++ resources/views/frontend/layouts/menu.blade.php
@@ -1,5 +1,5 @@
 <style>
-    .btn-close.background-dark-gray:hover {
+    #cross-nav:hover {
         background-color: black !important;
         /* Change to black on hover */
     }
@@ -80,7 +80,7 @@
                             <img src="{{ asset('uploads/web-logo1.png') }}" alt="3D-Cakes Logo"
                                 class="img-fluid head-img" />
                         </a>
-                        <button type="button" class="btn-close fs-4 background-dark-gray " data-bs-dismiss="offcanvas"
+                        <button id="cross-nav" type="button" class="btn-close fs-4 background-dark-gray " data-bs-dismiss="offcanvas"
                             aria-label="Close"></button>
                     </div>
                     <hr class="mx-3">
